2c7ce83ee354ed8b33f87381a10ffc5ca20d7ced
Enrico Zini <enrico@enricozini.org>
2006-01-17 18:40:30 +0000

    r6870@viaza:  enrico | 2006-01-17 19:40:04 +0100  Added test for generic messages  Added more conversions to support exports from VM
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@691 5c06e07b-eeef-0310-a3fe-9f68d50baefc

---
+++ tests/tests/dba_bufrex_generic.cc
@@ -0,0 +1,98 @@
+#include <tests/test-utils.h>
+#include <dballe/dba_file.h>
+#include <dballe/bufrex/bufrex.h>
+#include <dballe/bufrex/bufrex_raw.h>
+#include <dballe/msg/dba_msg.h>
+
+namespace tut {
+using namespace tut_dballe;
+
+struct dba_bufrex_generic_shar
+{
+	dba_bufrex_generic_shar()
+	{
+	}
+
+	~dba_bufrex_generic_shar()
+	{
+		test_untag();
+	}
+};
+TESTGRP(dba_bufrex_generic);
+
+template<> template<>
+void to::test<1>()
+{
+	dba_file file_gen;
+	dba_file file_synop;
+	int gfound, bfound;
+	dba_msg gen, synop;
+
+	CHECKED(dba_file_create(&file_gen, BUFR, "bufr/gen-generic.bufr", "r"));
+	CHECKED(dba_file_create(&file_synop, BUFR, "bufr/gen-synop.bufr", "r"));
+
+	CHECKED(dba_file_read(file_gen, &gen, &gfound));
+	CHECKED(dba_file_read(file_synop, &synop, &bfound));
+	gen_ensure_equals(gfound, bfound);
+	do {
+		gen_ensure_equals(gen->type, MSG_GENERIC);
+
+		/* Export gen as a synop message */
+		/* TODO: use the same template as the other synop message */
+		dba_rawmsg raw;
+		gen->type = MSG_SYNOP;
+		CHECKED(bufrex_encode_bufr(gen, 0, 0, &raw));
+
+		/* Parse the second dba_rawmsg */
+		dba_msg synop1;
+		CHECKED(bufrex_decode_bufr(raw, &synop1));
+
+		/*
+		if (string(files[i]).find("2-101.16") != string::npos)
+		{
+			FILE* outraw = fopen("/tmp/1to2.txt", "w");
+			bufrex_raw braw;
+			CHECKED(bufrex_raw_create(&braw, BUFREX_BUFR));
+			braw->type = type;
+			braw->subtype = subtype;
+			braw->opt.bufr.origin = 98;
+			braw->opt.bufr.master_table = 6;
+			braw->opt.bufr.local_table = 1;
+			CHECKED(bufrex_raw_load_tables(braw));
+			CHECKED(bufrex_raw_from_msg(braw, msg1));
+			bufrex_raw_print(braw, outraw);
+			fclose(outraw);
+			bufrex_raw_delete(braw);
+
+			FILE* out1 = fopen("/tmp/msg1.txt", "w");
+			FILE* out2 = fopen("/tmp/msg2.txt", "w");
+				
+			dba_msg_print(msg1, out1);
+			dba_msg_print(msg2, out2);
+			fclose(out1);
+			fclose(out2);
+		}
+		*/
+
+		/* Check that the data are the same */
+		int diffs = 0;
+		dba_msg_diff(synop, synop1, &diffs, stderr);
+		gen_ensure_equals(diffs, 0);
+
+		dba_msg_delete(gen);
+		dba_msg_delete(synop);
+		dba_msg_delete(synop1);
+		CHECKED(dba_file_read(file_gen, &gen, &gfound));
+		CHECKED(dba_file_read(file_synop, &synop, &bfound));
+		gen_ensure_equals(gfound, bfound);
+	} while (gfound);
+
+	dba_file_delete(file_gen);
+	dba_file_delete(file_synop);
+}
+
+/* TODO: add entries for more of the sample messages, taking data from another decoder */
+
+}
+
+/* vim:set ts=4 sw=4: */
