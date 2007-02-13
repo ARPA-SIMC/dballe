5d90d6e613b98f7dbba342d4e0b256d1e4ddbbde
Enrico Zini <enrico@enricozini.org>
2007-02-13 17:38:18 +0000

    r2754@viaza:  enrico | 2007-02-13 18:40:01 +0100  Added dimnames to exported arrays
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@1629 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- swig/rconvert.py
+++ swig/rconvert.py
@@ -1,52 +1,58 @@
 # Failed experiment: the __dict__ of MaskedArray is a dictproxy instance that
 # is read-only
+import dballe.volnd
 import MA, numpy
 import rpy
 
+#dballe.volnd.Data.__dict__['as_r'] = new.instancemethod(fun, None, dballe.volnd.Data)
 
-_nan = float("NaN")
-
-class RConvertibleMaskedArray(MA.MA.MaskedArray):
-
-	def as_r(self):
-		"""
-		Convert to an R object
-		"""
-		#temp = numpy.array(self.filled(), dtype=float)
-
-		# To compensate for unconvertible typecodes, various fill
-		# values and whatnot, we just create a new array filling in
-		# with NaNs as needed
-		#res = numpy.fromiter(self._gen_floats(), dtype=float, count=numpy.prod(self.shape))
-		len = numpy.prod(self.shape)
-		oldmode = rpy.r.rep.local_mode()
-		rpy.r.rep.local_mode(rpy.NO_CONVERSION)
-		rpy.r.array.local_mode(rpy.NO_CONVERSION)
-		rpy.r.aperm.local_mode(rpy.NO_CONVERSION)
-		vec = rpy.r.rep(_nan, len)
-		print "pre",; rpy.r.print_(vec)
-		for i, x in enumerate(self.flat):
-			print x
-			if MA.getmask(x) != 1:
-				vec[i] = float(x)
-		print "post",; rpy.r.print_(vec)
-		return rpy.r.aperm(rpy.r.array(data=vec, dim=[i for i in reversed(self.shape)]))
-
-		#return numpy.fromfunction(lambda args: _to_float_array(self, args), self.shape)
-
-
-def makeConvertible(obj):
-	if isinstance(obj, MA.MA.MaskedArray):
-		if hasattr(obj, 'as_r'):
-			# Don't mutate objects that already have an as_r method
-			pass
+def genfloat(a):
+	for x in a.flat:
+		if MA.getmask(x) != 1:
+			yield float(x)
 		else:
-			obj.__class__ = RConvertibleMaskedArray
+			yield rpy.r.NAN
 
-#setattr(obj, 'as_r', new.instancemethod(as_r, obj, obj.__class__))
+def ma_to_r(arr, dimnames=None):
+	"""
+	Convert to an R object
+	"""
 
-#if 'as_r' in MA.MA.MaskedArray.__dict__:
-#	print "Skipping duplicate"
-#else:
-#	MA.MA.MaskedArray.__dict__['as_r'] = 
+	vlen = numpy.prod(arr.shape)
 
+	### This strategy fails as it gets converted as a recursive list and
+	### not an array
+	## To compensate for unconvertible typecodes, various fill
+	## values and whatnot, we just create a new array filling in
+	## with proper NaNs as needed
+	#var = numpy.fromiter(genfloat(arr), dtype=float, count=len)
+	#var.shape = arr.shape
+	#rpy.r.print_(var)
+	#return var
+
+	# Trouble:
+	#  - a numpy array is converted as a list of lists
+	#  - I found no way to assign to an element in an R array
+	#  - cannot use a tuple to index a numpy/MA array
+
+	oldmode = rpy.r.rep.local_mode()
+	rpy.r.rep.local_mode(rpy.NO_CONVERSION)
+	rpy.r.array.local_mode(rpy.NO_CONVERSION)
+	rpy.r.aperm.local_mode(rpy.NO_CONVERSION)
+	vec = rpy.r.rep(rpy.r.NAN, vlen)
+	#print "pre",; rpy.r.print_(vec)
+	for i, x in enumerate(arr.flat):
+		if MA.getmask(x) != 1:
+			vec[i] = float(x)
+	#print "post",; rpy.r.print_(vec)
+	#return rpy.r.array(data=vec, dim=[i for i in arr.shape])
+	if dimnames:
+		return rpy.r.aperm(rpy.r.array(data=vec, dim=[i for i in reversed(arr.shape)], dimnames=[i for i in reversed(dimnames)]), perm=[i for i in reversed(range(1,len(arr.shape)+1))])
+	else:
+		return rpy.r.aperm(rpy.r.array(data=vec, dim=[i for i in reversed(arr.shape)]), perm=[i for i in reversed(range(1,len(arr.shape)+1))])
+
+def vnddata_to_r(data):
+	dn = []
+	for i in data.dims:
+		dn.append(map(str, i))
+	return ma_to_r(data.vals, dimnames=dn)
