3c030617029463e415113105c8fd63c9675a75d4
Enrico Zini <enrico@enricozini.org>
2007-02-27 10:21:54 +0000

    r2780@viaza:  enrico | 2007-02-14 11:12:15 +0100  Split gui bits out of provami.py  Added a way to autogenerate the manpage with help2man  Have provami.py not complain if wx or the provami modules are not installed,    and at least let --help work
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@1648 5c06e07b-eeef-0310-a3fe-9f68d50baefc

---
+++ provami/provami/ProgressDisplay.py
@@ -0,0 +1,30 @@
+import wx
+from provami.Model import Model, ProgressListener
+
+class ProgressDisplay(ProgressListener):
+	def __init__(self, parent, model):
+		self.parent = parent
+		self.model = model
+		self.model.registerProgressListener(self)
+		self.dlg = None
+
+	def progress(self, perc, text):
+		print "%d%%: %s" % (perc, text)
+		if perc == 100:
+			if self.dlg != None:
+				self.dlg.Destroy()
+				self.dlg = None
+		else:
+			if self.dlg == None:
+				self.dlg = wx.ProgressDialog("Updating data",
+						text,
+						maximum = 100,
+						parent = self.parent,
+						style = wx.PD_CAN_ABORT
+							| wx.PD_APP_MODAL
+							| wx.PD_ELAPSED_TIME
+							| wx.PD_ESTIMATED_TIME
+							| wx.PD_REMAINING_TIME)
+			if not self.dlg.Update(perc, text):
+				self.model.cancelUpdate()
+		wx.Yield()
