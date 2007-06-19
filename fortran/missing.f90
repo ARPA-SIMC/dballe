7af04de1bcf37f77fee3a881c70def0b77b8b679
Enrico Zini <enrico@enricozini.org>
2007-06-19 16:03:32 +0000

    r1909@viaza:  enrico | 2007-06-19 17:03:30 +0100  No use of real*8
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@1884 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- fortran/missing.f90
+++ fortran/missing.f90
@@ -9,6 +9,6 @@
       integer :: fortran_missing_int = DBA_MVI
       integer (kind=dba_int_b) :: fortran_missing_byte = DBA_MVB
       real :: fortran_missing_real = DBA_MVR
-      real*8 :: fortran_missing_double = DBA_MVD
+      real(kind=dba_fp_d) :: fortran_missing_double = DBA_MVD
 
       end module missing
