12598ab265304a55c0c0d4f11bfd04a779592eeb
Enrico Zini <enrico@enricozini.org>
2007-08-31 10:31:13 +0000

    r2487@viaza:  enrico | 2007-08-31 12:17:41 +0200  Split common check utils in a separate included file
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@1942 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- fortran/check_attrs.f90
+++ fortran/check_attrs.f90
@@ -1,8 +1,8 @@
       program check_attrs
 
-ccc *****************************************
-ccc * Dump the contents of a dballe database
-ccc *****************************************
+! *****************************************
+! * Dump the contents of a dballe database
+! *****************************************
 
       integer dbahandle, handle, handleinit, nstaz, ndata, nattr
       integer i, i1, i2, tmp
@@ -12,20 +12,20 @@ ccc *****************************************
       real dlat,dlon
       external errorrep
 
-c      call idba_error_set_callback(0, errorrep, 2, i)
+!      call idba_error_set_callback(0, errorrep, 2, i)
 
-c     Database login
+!     Database login
       call idba_presentati(dbahandle, "test", "enrico", "")
       call ensure_no_error("presentati")
 
-c     Open a session
+!     Open a session
       call idba_preparati(dbahandle, handle, "read", "read", "read")
       call ensure_no_error("preparati handle")
-      call idba_preparati(dbahandle, handleinit,
-     $  "write", "write", "write")
+      call idba_preparati(dbahandle, handleinit, &
+             "write", "write", "write")
       call ensure_no_error("preparati handleinit")
 
-c     Insert test data
+!     Insert test data
       call idba_setd(handleinit, "lat", 12.345D00)
       call ensure_no_error("init 1")
       call idba_setd(handleinit, "lon", 12.345D00)
@@ -100,72 +100,5 @@ c     Insert test data
     
       end
 
-ccc ********************
-ccc * Utility functions
-ccc ********************
+      include "check-utils.h"
 
-c     Compute the length of a string
-      integer function istrlen(string)
-      character string*(*)
-      istrlen = len(string)
-      do while ((string(istrlen:istrlen).eq." " .or.
-     $     string(istrlen:istrlen).eq."").and.
-     $     istrlen.gt.0)
-         istrlen = istrlen - 1
-      enddo
-      return
-      end
-
-      subroutine errorrep(val)
-      integer val
-      character buf*1000
-
-      ier = idba_error_code()
-      if (ier.ne.0) then
-         print *,ier," error in ",val
-         call idba_error_message(buf)
-         print *,buf(:istrlen(buf))
-         call idba_error_context(buf)
-         print *,buf(:istrlen(buf))
-         call idba_error_details(buf)
-         print *,buf(:istrlen(buf))
-         call exit (1)
-      end if
-      return
-
-      end
-
-c     Print an error if the given logical value is false
-      subroutine ensure(message, value)
-      character message*(*)
-      logical value
-
-      if (.not.value) then
-         print *,"Check failed in ",message
-         call exit (1)
-      end if
-      return
-
-      end
-
-c     Continue execution only if there was no error
-      subroutine ensure_no_error(message)
-      character message*(*)
-      integer idba_error_code, ier
-      character buf*1000
-
-c      print *,"siamo a ",message
-      ier = idba_error_code()
-      if (ier.ne.0) then
-         print *,ier," in ",message
-         call idba_error_message(buf)
-         print *,buf(:istrlen(buf))
-         call idba_error_context(buf)
-         print *,buf(:istrlen(buf))
-         call idba_error_details(buf)
-         print *,buf(:istrlen(buf))
-         call exit (1)
-      end if
-      return
-
-      end
