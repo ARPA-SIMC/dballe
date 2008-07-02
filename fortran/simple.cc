5a5e2ab1f726d85213abc288417cc436402cb46e
Enrico Zini <enrico@enricozini.org>
2008-07-02 14:57:03 +0000

    r2200@viaza:  enrico | 2008-07-02 10:38:53 +0000  Separate the API implementation from the Fortran bindings: extracted common parts
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@2169 5c06e07b-eeef-0310-a3fe-9f68d50baefc

---
+++ fortran/simple.cc
@@ -0,0 +1,37 @@
+/*
+ * DB-ALLe - Archive for punctual meteorological data
+ *
+ * Copyright (C) 2005--2008  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Author: Enrico Zini <enrico@enricozini.com>
+ */
+
+#include "simple.h"
+
+#include <limits>
+
+using namespace std;
+
+namespace dballef {
+
+const signed char API::missing_byte = numeric_limits<signed char>::min();
+const int API::missing_int = numeric_limits<int>::min();
+const float API::missing_float = numeric_limits<float>::max();
+const double API::missing_double = numeric_limits<double>::max();
+
+}
+
+/* vim:set ts=4 sw=4: */
