42c161459ef8d4ef79d863e6da200d19becd5113
Enrico Zini <enrico@enricozini.org>
2010-09-20 12:48:46 +0000

    Retabbed
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@2714 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/msg/wr_exporters/metar.cc
+++ dballe/msg/wr_exporters/metar.cc
@@ -74,17 +74,17 @@ struct Metar : public Template
 
         // Data descriptor section
         bulletin.datadesc.clear();
-	bulletin.datadesc.push_back(WR_VAR(3,  7,  11));
+        bulletin.datadesc.push_back(WR_VAR(3,  7,  11));
         if (!is_crex)
-	{
-		bulletin.datadesc.push_back(WR_VAR(2, 22,   0));
-		bulletin.datadesc.push_back(WR_VAR(1,  1,  21));
-		bulletin.datadesc.push_back(WR_VAR(0, 31,  31));
-		bulletin.datadesc.push_back(WR_VAR(0,  1,  31));
-		bulletin.datadesc.push_back(WR_VAR(0,  1,  32));
-		bulletin.datadesc.push_back(WR_VAR(1,  1,  21));
-		bulletin.datadesc.push_back(WR_VAR(0, 33,   7));
-	}
+        {
+                bulletin.datadesc.push_back(WR_VAR(2, 22,   0));
+                bulletin.datadesc.push_back(WR_VAR(1,  1,  21));
+                bulletin.datadesc.push_back(WR_VAR(0, 31,  31));
+                bulletin.datadesc.push_back(WR_VAR(0,  1,  31));
+                bulletin.datadesc.push_back(WR_VAR(0,  1,  32));
+                bulletin.datadesc.push_back(WR_VAR(1,  1,  21));
+                bulletin.datadesc.push_back(WR_VAR(0, 33,   7));
+        }
 
         bulletin.load_tables();
     }
@@ -92,27 +92,27 @@ struct Metar : public Template
     {
         Template::to_subset(msg, subset);
 
-	/*  0 */ add(WR_VAR(0,  1, 63), DBA_MSG_ST_NAME_ICAO);
-	/*  1 */ add(WR_VAR(0,  2,  1), DBA_MSG_ST_TYPE);
-	/*  2 */ add(WR_VAR(0,  4,  1), DBA_MSG_YEAR);
-	/*  3 */ add(WR_VAR(0,  4,  2), DBA_MSG_MONTH);
-	/*  4 */ add(WR_VAR(0,  4,  3), DBA_MSG_DAY);
-	/*  5 */ add(WR_VAR(0,  4,  4), DBA_MSG_HOUR);
-	/*  6 */ add(WR_VAR(0,  4,  5), DBA_MSG_MINUTE);
-	/*  7 */ add(WR_VAR(0,  5,  2), DBA_MSG_LATITUDE);
-	/*  8 */ add(WR_VAR(0,  6,  2), DBA_MSG_LONGITUDE);
-	/*  9 */ add(WR_VAR(0,  7,  1), DBA_MSG_HEIGHT);
-	/* 10 */ subset.store_variable_i(WR_VAR(0,  7,  6), 10);
-	/* 11 */ add(WR_VAR(0, 11,  1), DBA_MSG_WIND_DIR);
-	/* 12 */ add(WR_VAR(0, 11, 16), DBA_MSG_EX_CCW_WIND);
-	/* 13 */ add(WR_VAR(0, 11, 17), DBA_MSG_EX_CW_WIND);
-	/* 14 */ add(WR_VAR(0, 11,  2), DBA_MSG_WIND_SPEED);
-	/* 15 */ add(WR_VAR(0, 11, 41), DBA_MSG_WIND_MAX);
-	/* 16 */ subset.store_variable_i(WR_VAR(0,  7,  6), 2);
-	/* 15 */ add(WR_VAR(0, 12,  1), DBA_MSG_TEMP_2M);
-	/* 16 */ add(WR_VAR(0, 12,  3), DBA_MSG_DEWPOINT_2M);
-	/* 17 */ add(WR_VAR(0, 10, 52), DBA_MSG_QNH);
-	/* 18 */ add(WR_VAR(0, 20,  9), DBA_MSG_METAR_WTR);
+        /*  0 */ add(WR_VAR(0,  1, 63), DBA_MSG_ST_NAME_ICAO);
+        /*  1 */ add(WR_VAR(0,  2,  1), DBA_MSG_ST_TYPE);
+        /*  2 */ add(WR_VAR(0,  4,  1), DBA_MSG_YEAR);
+        /*  3 */ add(WR_VAR(0,  4,  2), DBA_MSG_MONTH);
+        /*  4 */ add(WR_VAR(0,  4,  3), DBA_MSG_DAY);
+        /*  5 */ add(WR_VAR(0,  4,  4), DBA_MSG_HOUR);
+        /*  6 */ add(WR_VAR(0,  4,  5), DBA_MSG_MINUTE);
+        /*  7 */ add(WR_VAR(0,  5,  2), DBA_MSG_LATITUDE);
+        /*  8 */ add(WR_VAR(0,  6,  2), DBA_MSG_LONGITUDE);
+        /*  9 */ add(WR_VAR(0,  7,  1), DBA_MSG_HEIGHT);
+        /* 10 */ subset.store_variable_i(WR_VAR(0,  7,  6), 10);
+        /* 11 */ add(WR_VAR(0, 11,  1), DBA_MSG_WIND_DIR);
+        /* 12 */ add(WR_VAR(0, 11, 16), DBA_MSG_EX_CCW_WIND);
+        /* 13 */ add(WR_VAR(0, 11, 17), DBA_MSG_EX_CW_WIND);
+        /* 14 */ add(WR_VAR(0, 11,  2), DBA_MSG_WIND_SPEED);
+        /* 15 */ add(WR_VAR(0, 11, 41), DBA_MSG_WIND_MAX);
+        /* 16 */ subset.store_variable_i(WR_VAR(0,  7,  6), 2);
+        /* 15 */ add(WR_VAR(0, 12,  1), DBA_MSG_TEMP_2M);
+        /* 16 */ add(WR_VAR(0, 12,  3), DBA_MSG_DEWPOINT_2M);
+        /* 17 */ add(WR_VAR(0, 10, 52), DBA_MSG_QNH);
+        /* 18 */ add(WR_VAR(0, 20,  9), DBA_MSG_METAR_WTR);
 
         if (!is_crex)
         {
