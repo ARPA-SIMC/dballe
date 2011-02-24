6a6fa49ea5f9a8cffde81b06b9dfc97ae9782c77
Enrico Zini <enrico@enricozini.org>
2011-02-24 13:23:47 +0000

    Take into account ground level for 0 sensor height imports
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@3024 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/msg/wr_importers/synop.cc
+++ dballe/msg/wr_importers/synop.cc
@@ -51,7 +51,8 @@ protected:
 
     void set_gen_sensor(const Var& var, Varcode code, const Level& defaultLevel, const Trange& trange)
     {
-        if (height_sensor == MISSING_SENSOR_H || defaultLevel == Level(103, height_sensor * 1000))
+        if (height_sensor == MISSING_SENSOR_H || defaultLevel == Level(103, height_sensor * 1000)
+                || (defaultLevel.ltype1 == 1 && height_sensor == 0))
             msg->set(var, code, defaultLevel, trange);
         else if (opts.simplified)
         {
