3ae2405e7d15e2f2514f0ee013ebefed940ccd21
Enrico Zini <enrico@enricozini.org>
2011-09-01 16:19:26 +0000

    Revert "Replaced dumpmsg_dballe.f90 with a better example from Paolo"
    This reverts commit 91065b7bf904314eab32707d9f1f2038c97d0dc7.
    
    Rationale: Paolo's source doesn't compile here.
    
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@3420 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- fortran/dumpmsg_dballe.f90
+++ fortran/dumpmsg_dballe.f90
@@ -1,94 +1,99 @@
-program dump_dballe
+      program dump_dballe
 
-! ****************************************************
-! * Dump the contents of a weather messages in a file
-! ****************************************************
+! *********************************************
+! * Dump the contents of a weather message file
+! *********************************************
 
-implicit none
+      integer handle, nstaz, ndata, nattr
+      integer i, i1, i2, tmp
+      integer id,height,codrete
+      character fname*256,cname*20,rete*20,value*255,avalue*255
+      character btable*10,starbtable*10
+      real*8 dlat,dlon
+      external errorrep
 
-integer :: handle, nstaz, ndata, nattr
-integer :: i, i1, i2, type1, l1, type2, l2
-integer ::  height, rep_cod, year, month, day, hour, minute, second
-character(len=255) :: fname,prettyvalue
-character(len=20)  :: cname, rep_memo,value, avalue
-character(len=10) :: btable, starbtable
-doubleprecision ::dlat,dlon
-
-include "dballe/dballef.h"
-external errorrep
-call getarg(1,fname)
-
-call idba_error_set_callback(0, errorrep, 0, i)
+      call idba_error_set_callback(0, errorrep, 2, i)
 
 !     Open a session
-call idba_messaggi(handle, fname, "r", "BUFR")
+      call getarg(1,fname)
+      call idba_messaggi(handle, fname, "r", "AUTO", 0)
 
 !     Query all the stations
-do while (.true.)
-  call idba_quantesono(handle, nstaz)
-  if (nstaz .eq. 0) exit
+      do while (.true.)
+        call idba_quantesono(handle, nstaz)
+        write (*,*) nstaz," stazioni:"
+        if (nstaz .eq. 0) exit
 
-  call idba_elencamele(handle)
-  call idba_enq(handle, "name", cname)
-  call idba_enq(handle, "lat", dlat)
-  call idba_enq(handle, "lon", dlon)
-  call idba_enq(handle, "height", height)
-  call idba_enq(handle,"rep_memo",rep_memo)
+        do i=1, nstaz
+          call idba_elencamele(handle)
+          call idba_enqc(handle, "name", cname)
+          call idba_enqi(handle, "ana_id", id)
+          call idba_enqd(handle, "lat", dlat)
+          call idba_enqd(handle, "lon", dlon)
+          call idba_enqi(handle, "height", height)
+          call idba_enqc(handle,"rep_memo",rete)
+          call idba_enqi(handle,"rep_cod",codrete)
+          write (*,*) "Staz ",id," (",dlat,",",dlon,") '", &
+              cname(:istrlen(cname)),"' h:",height, &
+              " ",rep_memo,":",rep_cod
+          call idba_seti(handle,"ana_id",id)
+          call idba_voglioquesto(handle,ndata)
+          write (*,*) " ",ndata," dati:"
+          do i1=1, ndata
+            call idba_dammelo(handle,btable)
+            call idba_enqc(handle,btable,value)
+            write (*,*) '  var ',btable(:istrlen(btable)),": ", &
+              value(:istrlen(btable))
 
-  write (*,*) "Staz: ",trim(cname)," (",dlat,",",dlon,")"," h:",height," network: ",rep_memo
-  !call idba_set(handle,"varlist","B12101,B11002") ! only on DB is valid
-
-  call idba_voglioquesto(handle,ndata)
-  !write (*,*) " ",ndata," dati:"                  ! only on DB is valid
-  do i1=1, ndata
-    call idba_dammelo(handle,btable)
-    if (btable /= "B12101" .and. btable /= "B11002") cycle 
-
-    call idba_enqdate(handle, year, month, day, hour, minute, second)
-    call idba_enqlevel(handle, type1, l1, type2, l2)
-    call idba_enq(handle,btable,value)
-
-    print*,"----"
-    write (*,*) "date time: ",year, month, day, hour, minute
-    call idba_spiegal(handle,type1,l1,type2,l2,prettyvalue)
-    write (*,*) trim(prettyvalue)
-    call idba_spiegab(handle,btable,value,prettyvalue)
-    write (*,*) trim(prettyvalue)
-    
-    call idba_voglioancora (handle,nattr)
-    if (nattr > 0) then
-      write (*,*) "   ",nattr," attributi:"
-      do i2=1, nattr
-        call idba_ancora(handle,starbtable)
-        call idba_enq(handle,starbtable,avalue)
-        write(*,*) "    attr ",trim(starbtable),": ",avalue
+            call idba_voglioancora (handle,nattr)
+            write (*,*) "   ",nattr," attributi:"
+            do i2=1, nattr
+              call idba_ancora(handle,starbtable)
+              call idba_enqc(handle,starbtable,avalue)
+              write(*,*) "    attr ",starbtable(:istrlen(starbtable)), &
+                ": ",avalue(:istrlen(avalue))
+            enddo
+          enddo
+        enddo
       enddo
-    end if
-  enddo
-enddo
 
-call idba_fatto(handle)
-      
-call exit (0)
+      call idba_fatto(handle)
 
-end program dump_dballe
+      call exit (0)
+    
+      end
 
-subroutine errorrep(val)
-integer :: val
-character(len=1000) :: buf
+! ********************
+! * Utility functions
+! ********************
 
-ier = idba_error_code()
-if (ier.ne.0) then
-  print *,ier," error in ",val
-  call idba_error_message(buf)
-  print *,trim(buf)
-  call idba_error_context(buf)
-  print *,trim(buf)
-  call idba_error_details(buf)
-  print *,trim(buf)
-  call exit (1)
-end if
-return
+!     Compute the length of a string
+      integer function istrlen(string)
+      character string*(*)
+      istrlen = len(string)
+      do while ((string(istrlen:istrlen).eq." " .or. &
+           string(istrlen:istrlen).eq."").and. &
+           istrlen.gt.0)
+         istrlen = istrlen - 1
+      enddo
+      return
+      end
 
-end subroutine errorrep
+      subroutine errorrep(val)
+      integer val
+      character buf*1000
 
+      ier = idba_error_code()
+      if (ier.ne.0) then
+         print *,ier," error in ",val
+         call idba_error_message(buf)
+         print *,buf(:istrlen(buf))
+         call idba_error_context(buf)
+         print *,buf(:istrlen(buf))
+         call idba_error_details(buf)
+         print *,buf(:istrlen(buf))
+         call exit (1)
+      end if
+      return
+
+      end
