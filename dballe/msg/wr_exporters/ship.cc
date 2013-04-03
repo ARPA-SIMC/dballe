54f9a4dd9b465c1ff1d40b7985250217331f1138
Enrico Zini <enrico@enricozini.org>
2013-04-03 12:32:26 +0000

    Activated ecmwf second record template
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@3865 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/msg/wr_exporters/ship.cc
+++ dballe/msg/wr_exporters/ship.cc
@@ -32,6 +32,9 @@ using namespace std;
 #define SHIP_PLAIN_NAME "ship-plain"
 #define SHIP_PLAIN_DESC "Synop ship (normal) (1.11)"
 
+#define SHIP_ECMWF_SECOND_NAME "ship-second"
+#define SHIP_ECMWF_SECOND_DESC "Synop ship (second record) (1.12)"
+
 #define SHIP_ABBR_NAME "ship-abbr"
 #define SHIP_ABBR_DESC "Synop ship (abbreviated) (1.9)"
 
@@ -242,6 +245,9 @@ struct ShipECMWFSecondRecord : public ShipBase
     ShipECMWFSecondRecord(const Exporter::Options& opts, const Msgs& msgs)
         : ShipBase(opts, msgs) {}
 
+    virtual const char* name() const { return SHIP_ECMWF_SECOND_NAME; }
+    virtual const char* description() const { return SHIP_ECMWF_SECOND_DESC; }
+
     virtual void setupBulletin(wreport::Bulletin& bulletin)
     {
         ShipBase::setupBulletin(bulletin);
@@ -255,6 +261,7 @@ struct ShipECMWFSecondRecord : public ShipBase
         bulletin.type = 1;
         bulletin.subtype = 255;
         bulletin.localsubtype = 12;
+        bulletin.load_tables();
 
         // Data descriptor section
         bulletin.datadesc.clear();
@@ -298,10 +305,12 @@ struct ShipECMWFSecondRecord : public ShipBase
         synop.add_year_to_minute();
         synop.add_latlon_coarse();
 
+        // TODO
         subset.store_variable_undef(WR_VAR(0, 12,  15)); // MINIMUM TEMPERATURE AT 2M, PAST 12 HOURS
         subset.store_variable_undef(WR_VAR(0, 12,  14)); // MAXIMUM TEMPERATURE AT 2M, PAST 12 HOURS
         synop.add_D02024();
         synop.add_plain_waves();
+        // TODO
         subset.store_variable_undef(WR_VAR(0, 13,  31)); // EVAPOTRANSPIRATION
         subset.store_variable_undef(WR_VAR(0, 14,  15)); // NET RADIATION INTEGRATED OVER 24HOURS
         subset.store_variable_undef(WR_VAR(0, 14,  31)); // TOTAL SUNSHINE
@@ -414,14 +423,49 @@ struct ShipFactory : public TemplateFactory
     std::auto_ptr<Template> make(const Exporter::Options& opts, const Msgs& msgs) const
     {
         // Scan msgs and pick the right one
+        bool maybe_wmo = true;
+        bool maybe_plain = true;
+        bool maybe_auto = true;
+        bool maybe_second = true;
         const Msg& msg = *msgs[0];
-        if (msg.find(WR_VAR(0, 2, 2), Level::ana(), Trange::ana()))
-            return auto_ptr<Template>(new ShipWMO(opts, msgs));
-        const Var* var = msg.get_st_type_var();
-        if (var == NULL || var->enqi() == 1)
+        for (std::vector<msg::Context*>::const_iterator i = msg.data.begin();
+                i != msg.data.end(); ++i)
+        {
+            const msg::Context& c = **i;
+            switch (c.level.ltype1)
+            {
+                case 257:
+                    for (std::vector<wreport::Var*>::const_iterator vi = c.data.begin();
+                            vi != c.data.end(); ++vi)
+                    {
+                        switch ((*vi)->code())
+                        {
+                            case WR_VAR(0, 2, 1):
+                                switch ((*vi)->enq(0))
+                                {
+                                    case 0: maybe_plain = false; break;
+                                    case 1: maybe_auto = false; break;
+                                }
+                                break;
+                            case WR_VAR(0, 2, 2):
+                                maybe_plain = maybe_auto = maybe_second = false;
+                                break;
+                        }
+                    }
+                    break;
+                case 264: maybe_plain = maybe_auto = false; break;
+            }
+        }
+        //if (maybe_wmo)
+        //    return auto_ptr<Template>(new ShipWMO(opts, msgs));
+        if (maybe_plain)
             return auto_ptr<Template>(new ShipPlain(opts, msgs));
-        else
+        if (maybe_auto)
             return auto_ptr<Template>(new ShipAuto(opts, msgs));
+        if (maybe_second)
+            return auto_ptr<Template>(new ShipECMWFSecondRecord(opts, msgs));
+        // Fallback on WMO if we are confused
+        return auto_ptr<Template>(new ShipWMO(opts, msgs));
     }
 };
 
@@ -432,6 +476,15 @@ struct ShipPlainFactory : public TemplateFactory
     std::auto_ptr<Template> make(const Exporter::Options& opts, const Msgs& msgs) const
     {
         return auto_ptr<Template>(new ShipPlain(opts, msgs));
+    }
+};
+struct ShipECMWFSecondRecordFactory : public TemplateFactory
+{
+    ShipECMWFSecondRecordFactory() { name = SHIP_ECMWF_SECOND_NAME; description = SHIP_ECMWF_SECOND_DESC; }
+
+    std::auto_ptr<Template> make(const Exporter::Options& opts, const Msgs& msgs) const
+    {
+        return auto_ptr<Template>(new ShipECMWFSecondRecord(opts, msgs));
     }
 };
 struct ShipAbbrFactory : public TemplateFactory
@@ -477,6 +530,7 @@ void register_ship(TemplateRegistry& r)
 {
 static const TemplateFactory* ship = NULL;
 static const TemplateFactory* shipplain = NULL;
+static const TemplateFactory* shipsecond = NULL;
 static const TemplateFactory* shipabbr = NULL;
 static const TemplateFactory* shipauto = NULL;
 static const TemplateFactory* shipreduced = NULL;
@@ -484,6 +538,7 @@ static const TemplateFactory* shipwmo = NULL;
 
     if (!ship) ship = new ShipFactory;
     if (!shipplain) shipplain = new ShipPlainFactory;
+    if (!shipsecond) shipsecond = new ShipECMWFSecondRecordFactory;
     if (!shipabbr) shipabbr = new ShipAbbrFactory;
     if (!shipauto) shipauto = new ShipAutoFactory;
     if (!shipreduced) shipreduced = new ShipReducedFactory;
@@ -491,6 +546,7 @@ static const TemplateFactory* shipwmo = NULL;
 
     r.register_factory(ship);
     r.register_factory(shipplain);
+    r.register_factory(shipsecond);
     r.register_factory(shipabbr);
     r.register_factory(shipauto);
     r.register_factory(shipreduced);
