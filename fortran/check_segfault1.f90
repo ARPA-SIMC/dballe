4f1515c1ccbb917bee43ab5b5ecd4f328360c274
Enrico Zini <enrico@enricozini.org>
2013-06-18 16:24:41 +0000

    Error handling, and slightly more readable var names
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@3900 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- fortran/check_segfault1.f90
+++ fortran/check_segfault1.f90
@@ -1,66 +1,68 @@
 program test
 
 
-integer :: idbhandle,handle,handle_,i
-integer :: idbhandle2,handle2,handle2_,anaid
+integer :: idbhandle1,handle1,handle1a,i
+integer :: idbhandle2,handle2,handle2a,anaid
+integer :: error_handle
 
 
 include "dballef.h"
 
+call idba_error_set_callback(0, idba_default_error_handler, 42, error_handle)
 
 
 do i =1, 2000
 
-  call idba_presentati(idbhandle,"sqlite:tmp.sqlite","","")
-!  print *,"Presentati",idbhandle
-  call idba_preparati (idbhandle,handle,"write","write","write")
-!  print *,"Preparati",handle
-  call idba_preparati (idbhandle,handle_,"write","write","write")
-!  print *,"Preparati_",handle_
+  call idba_presentati(idbhandle1,"sqlite:tmp.sqlite","","")
+!  print *,"Presentati",idbhandle1
+  call idba_preparati (idbhandle1,handle1,"write","write","write")
+!  print *,"Preparati",handle1
+  call idba_preparati (idbhandle1,handle1a,"write","write","write")
+!  print *,"Preparati_",handle1a
 
   call idba_presentati(idbhandle2,"sqlite:tmp.sqlite","","")
 !  print *,"Presentati2",idbhandle2
   call idba_preparati (idbhandle2,handle2,"write","write","write")
 !  print *,"Preparati2",handle2
-  call idba_preparati (idbhandle2,handle2_,"write","write","write")
-!  print *,"Preparati2",handle2_
+  call idba_preparati (idbhandle2,handle2a,"write","write","write")
+!  print *,"Preparati2",handle2a
 
 
 
 !     Insert data about a station
-  call idba_set (handle, "lat", 11.345)
-  call idba_set (handle, "lon", 44.678)
-  call idba_set (handle, "height", 23)
-  call idba_prendilo (handle)
+  call idba_set (handle1, "lat", 11.345)
+  call idba_set (handle1, "lon", 44.678)
+  call idba_set (handle1, "height", 23)
+  call idba_prendilo (handle1)
 
 !     Read the station ID for the station we just inserted
-  call idba_enq (handle, "ana_id", anaid)
+  call idba_enq (handle1, "ana_id", anaid)
 
 !     Reset the input data
-  call idba_unsetall (handle)
+  call idba_unsetall (handle1)
 
 !     Add data to the station we just inserted
-  call idba_set (handle, "ana_id", anaid)
-  call idba_setlevel (handle, 100, 1, 0, 0)
-  call idba_settimerange (handle, 0, 0, 0)
-  call idba_setdate (handle, 2006, 06, 20, 19, 30, 0)
-  call idba_seti (handle, "t", 21)
-  call idba_setc (handle, "B12345", "ciao")
-  call idba_prendilo (handle)
+  call idba_set (handle1, "ana_id", anaid)
+  call idba_setlevel (handle1, 100, 1, 0, 0)
+  call idba_settimerange (handle1, 0, 0, 0)
+  call idba_setdate (handle1, 2006, 06, 20, 19, 30, 0)
+  call idba_seti (handle1, "t", 21)
+  call idba_setc (handle1, "B12345", "ciao")
+  call idba_prendilo (handle1)
  
 !  print *,"Fatto2",handle2
   call idba_fatto(handle2)
-!  print *,"Fatto2_",handle2_
-  call idba_fatto(handle2_)
+!  print *,"Fatto2_",handle2a
+  call idba_fatto(handle2a)
 !  print *,"Arrivederci2",idbhandle2
   call idba_arrivederci(idbhandle2)
 
-!  print *,"Fatto_",handle_
-  call idba_fatto(handle_)
-!  print *,"Fatto",handle
-  call idba_fatto(handle)
-!  print *,"Arrivederci",idbhandle
-  call idba_arrivederci(idbhandle)
+!  print *,"Fatto_",handle1a
+  call idba_fatto(handle1a)
+!  print *,"Fatto",handle1
+  call idba_fatto(handle1)
+!  print *,"Arrivederci",idbhandle1
+  call idba_arrivederci(idbhandle1)
 end do
 
 end program test
