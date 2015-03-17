f24b0627e0bcc94ec5ae57f5af275da74e349244
Enrico Zini <enrico@enricozini.org>
2015-03-17 09:36:44 +0000

    Added test to check behaviour with station values in different networks
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@4528 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/db/db-basic-tut.cc
+++ dballe/db/db-basic-tut.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ * Copyright (C) 2013--2015  ARPA-SIM <urpsim@smr.arpa.emr.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 
 #include "config.h"
 #include "db/test-utils-db.h"
+#include "db/mem/db.h"
 
 using namespace dballe;
 using namespace dballe::db;
@@ -97,6 +98,86 @@ template<> template<> void to::test<4>()
 
     cur = db->query_data(query);
     wassert(actual(cur->remaining()) == 0);
+}
+
+// Test adding station data for different networks
+template<> template<> void to::test<5>()
+{
+    db->reset();
+
+    Record rec;
+
+    rec.set(DBA_KEY_LAT, 12.077);
+    rec.set(DBA_KEY_LON, 44.600);
+
+    // Insert two values in two networks
+    rec.set(Level(103, 2000));
+    rec.set(Trange::instant());
+    rec.set(Datetime(2014, 1, 1, 0, 0, 0));
+    rec.set(DBA_KEY_REP_MEMO, "synop");
+    rec.set(WR_VAR(0, 12, 101), 273.15);
+    db->insert(rec, true, true);
+    rec.set(DBA_KEY_REP_MEMO, "temp");
+    rec.set(WR_VAR(0, 12, 101), 274.15);
+    db->insert(rec, true, true);
+
+    // Insert station names in both networks
+    rec.set_ana_context();
+    rec.set(DBA_KEY_REP_MEMO, "synop");
+    rec.set(WR_VAR(0, 1, 19), "Camse");
+    db->insert(rec, true, true);
+    rec.set(DBA_KEY_REP_MEMO, "temp");
+    rec.set(WR_VAR(0, 1, 19), "Esmac");
+    db->insert(rec, true, true);
+
+    //db->dump(stderr);
+
+    // Query back all the data
+    Query query;
+    auto cur = db->query_stations(query);
+
+    // Check results
+    Record result;
+    if (dynamic_cast<db::mem::DB*>(db.get()))
+    {
+        // For mem databases, we get one record per (station, network)
+        // combination
+        wassert(actual(cur->next()).istrue());
+        wassert(actual(cur->get_station_id()) == 1);
+        wassert(actual(cur->get_rep_memo()) == "temp");
+        cur->to_record(result);
+        wassert(actual(result.get(WR_VAR(0, 1, 19)).value()) == "Esmac");
+
+        wassert(actual(cur->next()).istrue());
+        wassert(actual(cur->get_station_id()) == 0);
+        wassert(actual(cur->get_rep_memo()) == "synop");
+        cur->to_record(result);
+        wassert(actual(result.get(WR_VAR(0, 1, 19)).value()) == "Camse");
+    } else {
+        // For normal databases, we only get one record, with the station
+        // values merged keeping values for the best networks
+        wassert(actual(cur->next()).istrue());
+        wassert(actual(cur->get_station_id()) == 1);
+        cur->to_record(result);
+        wassert(actual(result.get(WR_VAR(0, 1, 19)).value()) == "Camse");
+
+        wassert(actual(cur->next()).isfalse());
+    }
+
+    query.clear();
+    Msgs msgs;
+    msg::AcquireMessages amsg(msgs);
+    db->export_msgs(query, amsg);
+    wassert(actual(msgs.size()) == 2);
+
+    //msgs.print(stderr);
+
+    wassert(actual(msgs[0]->get_rep_memo_var()->enqc()) == "synop");
+    wassert(actual(msgs[0]->get_st_name_var()->enqc()) == "Camse");
+    wassert(actual(msgs[0]->get_temp_2m_var()->enqd()) == 273.15);
+    wassert(actual(msgs[1]->get_rep_memo_var()->enqc()) == "temp");
+    wassert(actual(msgs[1]->get_st_name_var()->enqc()) == "Esmac");
+    wassert(actual(msgs[1]->get_temp_2m_var()->enqd()) == 274.15);
 }
 
 }
