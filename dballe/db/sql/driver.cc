9d4dffaf5736c2d7fc7259f9cfd44d0112973b2d
Enrico Zini <enrico@enricozini.org>
2015-04-12 15:06:06 +0000

    Added some commented-out debugging instrumentation
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@4617 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/db/sql/driver.cc
+++ dballe/db/sql/driver.cc
@@ -68,15 +68,22 @@ namespace bulk {
 
 bool AnnotateVarsV6::annotate(int id_data, int id_levtr, Varcode code, const char* value)
 {
+    //fprintf(stderr, "ANNOTATE ");
     while (iter != vars.end())
     {
+        //fprintf(stderr, "id_data: %d/%d  id_levtr: %d/%d  varcode: %d/%d  value: %s/%s: ", id_data, iter->id_data, id_levtr, iter->id_levtr, code, iter->var->code(), value, iter->var->value());
+
         // This variable is not on our list: stop here and wait for a new one
         if (id_levtr < iter->id_levtr)
+        {
+            //fprintf(stderr, "levtr lower than ours, wait for next\n");
             return true;
+        }
 
         // iter points to a variable that is not currently in the DB
         if (id_levtr > iter->id_levtr)
         {
+            //fprintf(stderr, "levtr higher than ours, insert this\n");
             do_insert = true;
             iter->set_needs_insert();
             ++iter;
@@ -87,11 +94,15 @@ bool AnnotateVarsV6::annotate(int id_data, int id_levtr, Varcode code, const cha
 
         // This variable is not on our list: stop here and wait for a new one
         if (code < iter->var->code())
+        {
+            //fprintf(stderr, "varcode lower than ours, wait for next\n");
             return true;
+        }
 
         // iter points to a variable that is not currently in the DB
         if (code > iter->var->code())
         {
+            //fprintf(stderr, "varcode higher than ours, insert this\n");
             do_insert = true;
             iter->set_needs_insert();
             ++iter;
@@ -101,22 +112,26 @@ bool AnnotateVarsV6::annotate(int id_data, int id_levtr, Varcode code, const cha
         // iter points to a variable that is also in the DB
 
         // Annotate with the ID
+        //fprintf(stderr, "id_data=%d ", id_data);
         iter->id_data = id_data;
 
         // If the value is different, we need to update
         if (strcmp(value, iter->var->value()) != 0)
         {
+            //fprintf(stderr, "needs_update ");
             iter->set_needs_update();
             do_update = true;
         }
 
         // We processed this variable: stop here and wait for a new one
         ++iter;
+        //fprintf(stderr, "wait for next\n");
         return true;
     }
 
     // We have no more variables to consider: signal the caller that they can
     // stop iterating if they wish.
+    //fprintf(stderr, "done.\n");
     return false;
 }
 
@@ -125,6 +140,7 @@ void AnnotateVarsV6::annotate_end()
     // Mark all remaining variables as needing insert
     for ( ; iter != vars.end(); ++iter)
     {
+        //fprintf(stderr, "LEFTOVER: id_levtr: %d  varcode: %d  value: %s\n", iter->id_levtr, iter->var->code(), iter->var->value());
         iter->set_needs_insert();
         do_insert = true;
     }
