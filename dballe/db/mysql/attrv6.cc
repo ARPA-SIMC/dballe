37128bbeb04d9bcf3375a4b66dc919e92b22d5ab
Enrico Zini <enrico@enricozini.org>
2015-04-16 22:30:19 +0000

    Bulk attribute insert for MySQL
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@4653 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/db/mysql/attrv6.cc
+++ dballe/db/mysql/attrv6.cc
@@ -39,6 +39,73 @@ MySQLAttrV6::~MySQLAttrV6()
 {
 }
 
+void MySQLAttrV6::insert(Transaction& t, sql::bulk::InsertAttrsV6& attrs, UpdateMode update_mode)
+{
+    Querybuf select_query;
+    select_query.append("SELECT id_data, type, value FROM attr WHERE id_data IN (");
+    select_query.start_list(",");
+    int last_data_id = -1;
+    for (const auto& a: attrs)
+    {
+        if (a.id_data == last_data_id) continue;
+        select_query.append_listf("%d", a.id_data);
+        last_data_id = a.id_data;
+    }
+    select_query.append(") ORDER BY id_data, type");
+
+    // Get the current status of variables for this context
+    sql::bulk::AnnotateAttrsV6 todo(attrs);
+    auto select_res = conn.exec_store(select_query);
+    while (auto row = select_res.fetch())
+    {
+        todo.annotate(
+                row.as_int(0),
+                row.as_int(1),
+                row.as_cstring(2));
+    }
+    todo.annotate_end();
+
+    // We now have a todo-list
+
+    switch (update_mode)
+    {
+        case UPDATE:
+            if (todo.do_update)
+            {
+                for (auto& a: attrs)
+                {
+                    if (!a.needs_update()) continue;
+                    string escaped_value = conn.escape(a.attr->value());
+                    Querybuf update;
+                    update.appendf("UPDATE attr SET value='%s' WHERE id_data=%d AND type=%d", escaped_value.c_str(), a.id_data, (int)a.attr->code());
+                    conn.exec_no_data(update);
+                    a.set_updated();
+                }
+            }
+            break;
+        case IGNORE:
+            break;
+        case ERROR:
+            if (todo.do_update)
+                throw error_consistency("refusing to overwrite existing data");
+    }
+
+    if (todo.do_insert)
+    {
+        Querybuf insert(512);
+        insert.append("INSERT INTO attr (id_data, type, value) VALUES ");
+        insert.start_list(",");
+        for (auto& a: attrs)
+        {
+            if (!a.needs_insert()) continue;
+            string escaped_value = conn.escape(a.attr->value());
+            insert.append_listf("(%d, %d, '%s')", a.id_data, (int)a.attr->code(), escaped_value.c_str());
+            a.set_inserted();
+        }
+        conn.exec_no_data(insert);
+    }
+}
+
 void MySQLAttrV6::impl_add(int id_data, sql::AttributeList& attrs)
 {
     Querybuf q;
--- dballe/db/mysql/attrv6.h
+++ dballe/db/mysql/attrv6.h
@@ -47,6 +47,8 @@ public:
     MySQLAttrV6& operator=(const MySQLAttrV6&) = delete;
     ~MySQLAttrV6();
 
+    void insert(Transaction& t, sql::bulk::InsertAttrsV6& vars, UpdateMode update_mode=UPDATE) override;
+
     /**
      * Load from the database all the attributes for var
      *
