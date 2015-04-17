2715828bccaf6c0101867f9674639c02e69c6253
Enrico Zini <enrico@enricozini.org>
2015-04-17 14:16:28 +0000

    Ported export tests to the new test_group
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@4659 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/db/db-export-tut.cc
+++ dballe/db/db-export-tut.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005--2013  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ * Copyright (C) 2005--2015  ARPA-SIM <urpsim@smr.arpa.emr.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,8 +30,25 @@ using namespace std;
 
 namespace {
 
-struct db_export : public dballe::tests::db_test
+struct MsgCollector : public vector<Msg*>, public MsgConsumer
 {
+    ~MsgCollector()
+    {
+        for (iterator i = begin(); i != end(); ++i)
+            delete *i;
+    }
+    void operator()(unique_ptr<Msg> msg) override
+    {
+        push_back(msg.release());
+    }
+};
+
+struct Fixture : public dballe::tests::DBFixture
+{
+    Fixture()
+    {
+    }
+
     void populate_database()
     {
         using namespace dballe::tests;
@@ -72,148 +89,134 @@ struct db_export : public dballe::tests::db_test
     }
 };
 
-struct MsgCollector : public vector<Msg*>, public MsgConsumer
-{
-    ~MsgCollector()
-    {
-        for (iterator i = begin(); i != end(); ++i)
-            delete *i;
-    }
-    void operator()(unique_ptr<Msg> msg) override
-    {
-        push_back(msg.release());
-    }
+
+typedef dballe::tests::db_test_group<Fixture> test_group;
+typedef test_group::Test Test;
+
+std::vector<Test> tests {
+    Test("export", [](Fixture& f) {
+        // Simple export
+        auto& db = f.db;
+        f.populate_database();
+
+        // Put some data in the database and check that it gets exported properly
+        // Query back the data
+        Record query;
+
+        MsgCollector msgs;
+        db->export_msgs(query, msgs);
+        ensure_equals(msgs.size(), 4u);
+
+        if (msgs[2]->type == MSG_METAR)
+        {
+            // Since the order here is not determined, enforce one
+            Msg* tmp = msgs[2];
+            msgs[2] = msgs[3];
+            msgs[3] = tmp;
+        }
+
+        ensure_equals(msgs[0]->type, MSG_SYNOP);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_LATITUDE), 12.34560);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_LONGITUDE), 76.54321);
+        ensure_msg_undef(*msgs[0], DBA_MSG_IDENT);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_YEAR), 1945);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_MONTH), 4);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_DAY), 25);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_HOUR), 8);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_MINUTE), 0);
+        ensure_var_equals(want_var(*msgs[0], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 500);
+
+        ensure_equals(msgs[1]->type, MSG_SYNOP);
+        ensure_var_equals(want_var(*msgs[1], DBA_MSG_LATITUDE), 12.34560);
+        ensure_var_equals(want_var(*msgs[1], DBA_MSG_LONGITUDE), 76.54321);
+        ensure_msg_undef(*msgs[1], DBA_MSG_IDENT);
+        ensure_var_equals(want_var(*msgs[1], DBA_MSG_YEAR), 1945);
+        ensure_var_equals(want_var(*msgs[1], DBA_MSG_MONTH), 4);
+        ensure_var_equals(want_var(*msgs[1], DBA_MSG_DAY), 26);
+        ensure_var_equals(want_var(*msgs[1], DBA_MSG_HOUR), 8);
+        ensure_var_equals(want_var(*msgs[1], DBA_MSG_MINUTE), 0);
+        ensure_var_equals(want_var(*msgs[1], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 400);
+
+        ensure_equals(msgs[2]->type, MSG_SYNOP);
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_LATITUDE), 12.34560);
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_LONGITUDE), 76.54321);
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_IDENT), "ciao");
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_YEAR), 1945);
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_MONTH), 4);
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_DAY), 26);
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_HOUR), 8);
+        ensure_var_equals(want_var(*msgs[2], DBA_MSG_MINUTE), 0);
+        ensure_var_equals(want_var(*msgs[2], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 300);
+
+        ensure_equals(msgs[3]->type, MSG_METAR);
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_LATITUDE), 12.34560);
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_LONGITUDE), 76.54321);
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_IDENT), "ciao");
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_YEAR), 1945);
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_MONTH), 4);
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_DAY), 26);
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_HOUR), 8);
+        ensure_var_equals(want_var(*msgs[3], DBA_MSG_MINUTE), 0);
+        ensure_var_equals(want_var(*msgs[3], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 200);
+    }),
+    Test("export", [](Fixture& f) {
+        // Text exporting of extra station information
+        auto& db = f.db;
+
+        // Import some data in the station extra information context
+        Record in;
+        in.set(DBA_KEY_LAT, 45.0);
+        in.set(DBA_KEY_LON, 11.0);
+        in.set(DBA_KEY_REP_MEMO, "synop");
+        in.set_ana_context();
+        in.set(WR_VAR(0, 1, 1), 10);
+        db->insert(in, false, true);
+
+        // Import one real datum
+        in.clear();
+        in.set(DBA_KEY_LAT, 45.0);
+        in.set(DBA_KEY_LON, 11.0);
+        in.set(DBA_KEY_REP_MEMO, "synop");
+        in.set(DBA_KEY_YEAR, 2000);
+        in.set(DBA_KEY_MONTH, 1);
+        in.set(DBA_KEY_DAY, 1);
+        in.set(DBA_KEY_HOUR, 0);
+        in.set(DBA_KEY_MIN, 0);
+        in.set(DBA_KEY_SEC, 0);
+        in.set(DBA_KEY_LEVELTYPE1, 103);
+        in.set(DBA_KEY_L1, 2000);
+        in.set(DBA_KEY_PINDICATOR, 254);
+        in.set(DBA_KEY_P1, 0);
+        in.set(DBA_KEY_P2, 0);
+        in.set(WR_VAR(0, 12, 101), 290.0);
+        db->insert(in, false, true);
+
+        // Query back the data
+        Record query;
+        MsgCollector msgs;
+        db->export_msgs(query, msgs);
+        ensure_equals(msgs.size(), 1u);
+
+        ensure_equals(msgs[0]->type, MSG_SYNOP);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_LATITUDE), 45.0);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_LONGITUDE), 11.0);
+        ensure_msg_undef(*msgs[0], DBA_MSG_IDENT);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_BLOCK), 10);
+        ensure_var_equals(want_var(*msgs[0], DBA_MSG_TEMP_2M), 290.0);
+    }),
 };
 
-}
-
-namespace tut {
-
-using namespace dballe::tests;
-typedef db_tg<db_export> tg;
-typedef tg::object to;
-
-
-template<> template<> void to::test<1>()
-{
-    // Simple export
-    populate_database();
-
-    // Put some data in the database and check that it gets exported properly
-	// Query back the data
-    Record query;
-
-	MsgCollector msgs;
-    db->export_msgs(query, msgs);
-	ensure_equals(msgs.size(), 4u);
-
-	if (msgs[2]->type == MSG_METAR)
-	{
-		// Since the order here is not determined, enforce one
-		Msg* tmp = msgs[2];
-		msgs[2] = msgs[3];
-		msgs[3] = tmp;
-	}
-
-	ensure_equals(msgs[0]->type, MSG_SYNOP);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_LATITUDE), 12.34560);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_LONGITUDE), 76.54321);
-	ensure_msg_undef(*msgs[0], DBA_MSG_IDENT);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_YEAR), 1945);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_MONTH), 4);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_DAY), 25);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_HOUR), 8);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_MINUTE), 0);
-	ensure_var_equals(want_var(*msgs[0], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 500);
-
-	ensure_equals(msgs[1]->type, MSG_SYNOP);
-	ensure_var_equals(want_var(*msgs[1], DBA_MSG_LATITUDE), 12.34560);
-	ensure_var_equals(want_var(*msgs[1], DBA_MSG_LONGITUDE), 76.54321);
-	ensure_msg_undef(*msgs[1], DBA_MSG_IDENT);
-	ensure_var_equals(want_var(*msgs[1], DBA_MSG_YEAR), 1945);
-	ensure_var_equals(want_var(*msgs[1], DBA_MSG_MONTH), 4);
-	ensure_var_equals(want_var(*msgs[1], DBA_MSG_DAY), 26);
-	ensure_var_equals(want_var(*msgs[1], DBA_MSG_HOUR), 8);
-	ensure_var_equals(want_var(*msgs[1], DBA_MSG_MINUTE), 0);
-	ensure_var_equals(want_var(*msgs[1], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 400);
-
-	ensure_equals(msgs[2]->type, MSG_SYNOP);
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_LATITUDE), 12.34560);
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_LONGITUDE), 76.54321);
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_IDENT), "ciao");
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_YEAR), 1945);
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_MONTH), 4);
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_DAY), 26);
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_HOUR), 8);
-	ensure_var_equals(want_var(*msgs[2], DBA_MSG_MINUTE), 0);
-	ensure_var_equals(want_var(*msgs[2], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 300);
-
-	ensure_equals(msgs[3]->type, MSG_METAR);
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_LATITUDE), 12.34560);
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_LONGITUDE), 76.54321);
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_IDENT), "ciao");
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_YEAR), 1945);
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_MONTH), 4);
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_DAY), 26);
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_HOUR), 8);
-	ensure_var_equals(want_var(*msgs[3], DBA_MSG_MINUTE), 0);
-	ensure_var_equals(want_var(*msgs[3], WR_VAR(0, 1, 12), Level(1, 2, 0, 3), Trange(4, 5, 6)), 200);
-}
-
-template<> template<> void to::test<2>()
-{
-    // Text exporting of extra station information
-
-    // Import some data in the station extra information context
-    Record in;
-    in.set(DBA_KEY_LAT, 45.0);
-    in.set(DBA_KEY_LON, 11.0);
-    in.set(DBA_KEY_REP_MEMO, "synop");
-    in.set_ana_context();
-    in.set(WR_VAR(0, 1, 1), 10);
-    db->insert(in, false, true);
-
-    // Import one real datum
-    in.clear();
-    in.set(DBA_KEY_LAT, 45.0);
-    in.set(DBA_KEY_LON, 11.0);
-    in.set(DBA_KEY_REP_MEMO, "synop");
-    in.set(DBA_KEY_YEAR, 2000);
-    in.set(DBA_KEY_MONTH, 1);
-    in.set(DBA_KEY_DAY, 1);
-    in.set(DBA_KEY_HOUR, 0);
-    in.set(DBA_KEY_MIN, 0);
-    in.set(DBA_KEY_SEC, 0);
-    in.set(DBA_KEY_LEVELTYPE1, 103);
-    in.set(DBA_KEY_L1, 2000);
-    in.set(DBA_KEY_PINDICATOR, 254);
-    in.set(DBA_KEY_P1, 0);
-    in.set(DBA_KEY_P2, 0);
-    in.set(WR_VAR(0, 12, 101), 290.0);
-    db->insert(in, false, true);
-
-	// Query back the data
-    Record query;
-	MsgCollector msgs;
-    db->export_msgs(query, msgs);
-	ensure_equals(msgs.size(), 1u);
-
-	ensure_equals(msgs[0]->type, MSG_SYNOP);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_LATITUDE), 45.0);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_LONGITUDE), 11.0);
-	ensure_msg_undef(*msgs[0], DBA_MSG_IDENT);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_BLOCK), 10);
-	ensure_var_equals(want_var(*msgs[0], DBA_MSG_TEMP_2M), 290.0);
-}
-
-}
-
-namespace {
-
-tut::tg db_export_mem_tg("db_export_mem", MEM);
+test_group tg1("db_export_mem", nullptr, db::MEM, tests);
+test_group tg2("db_export_v6_sqlite", "SQLITE", db::V6, tests);
 #ifdef HAVE_ODBC
-tut::tg db_export_v5_tg("db_export_v5", V5);
-tut::tg db_export_v6_tg("db_export_v6", V6);
+test_group tg3("db_export_v5_odbc", "ODBC", db::V5, tests);
+test_group tg4("db_export_v6_odbc", "ODBC", db::V6, tests);
+#endif
+#ifdef HAVE_LIBPQ
+test_group tg6("db_export_v6_postgresql", "POSTGRESQL", db::V6, tests);
+#endif
+#ifdef HAVE_MYSQL
+test_group tg8("db_export_v6_mysql", "MYSQL", db::V6, tests);
 #endif
 
 }
