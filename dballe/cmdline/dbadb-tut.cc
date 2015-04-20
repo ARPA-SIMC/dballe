c60c4afedd5d3bc6011343ce5ac5e73313c1be3b
Enrico Zini <enrico@enricozini.org>
2015-04-20 15:26:56 +0000

    Converted cmdline tests to new test_group
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@4667 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/cmdline/dbadb-tut.cc
+++ dballe/cmdline/dbadb-tut.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005--2013  ARPA-SIM <urpsim@smr.arpa.emr.it>
+ * Copyright (C) 2005--2015  ARPA-SIM <urpsim@smr.arpa.emr.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,86 +26,79 @@
 using namespace dballe;
 using namespace dballe::cmdline;
 using namespace wreport;
+using namespace wibble::tests;
 using namespace std;
-
-namespace dballe {
-namespace tests {
-
-struct test_dbadb : public db_test
-{
-};
-
-}
-}
-
-namespace tut {
-
-using namespace dballe::tests;
-typedef db_tg<test_dbadb> tg;
-typedef tg::object to;
-
-template<> template<> void to::test<1>()
-{
-    // Resolve NULL and the empty string
-    ensure_equals(dbadb::parse_op_report(*db, NULL), (const char*)NULL);
-    ensure_equals(dbadb::parse_op_report(*db, ""), (const char*)NULL);
-
-    // Resolve from names
-    ensure_equals(string(dbadb::parse_op_report(*db, "synop")), "synop");
-
-    // Resolve from nonexisting numbers
-    try {
-        dbadb::parse_op_report(*db, "11234");
-        ensure(false);
-    } catch (...) {}
-
-    // Resolve from nonexisting names
-    try {
-        dbadb::parse_op_report(*db, "lolcats");
-        ensure(false);
-    } catch (...) {}
-}
-
-template<> template<> void to::test<2>()
-{
-    Dbadb dbadb(*db);
-
-    // Import a synop
-    cmdline::Reader reader;
-    ensure_equals(dbadb.do_import(dballe::tests::datafile("bufr/obs0-1.22.bufr"), reader), 0);
-
-    // Export forcing report as temp
-    Record query;
-    ArrayFile file(BUFR);
-    ensure_equals(dbadb.do_export(query, file, "generic", "ship"), 0);
-
-    ensure_equals(file.msgs.size(), 1u);
-
-    // Decode results
-    auto importer = msg::Importer::create(BUFR);
-    Msgs msgs;
-    importer->from_rawmsg(file.msgs[0], msgs);
-    ensure_equals(msgs.size(), 1u);
-
-    // Ensure they're ships
-    ensure_equals(msgs[0]->type, MSG_SHIP);
-
-    // Check 001194 [SIM] Report mnemonic(CCITTIA5), too
-    const Var* var = msgs[0]->get_rep_memo_var();
-    ensure(var != 0);
-    ensure(var->enqc() != 0);
-    ensure_equals(var->enq<std::string>(), "ship");
-}
-
-}
 
 namespace {
 
-tut::tg tests_dbadb_mem_tg("cmdline_dbadb_mem", db::MEM);
+typedef dballe::tests::DBFixture Fixture;
+typedef dballe::tests::db_test_group<Fixture> test_group;
+typedef test_group::Test Test;
+
+std::vector<Test> tests {
+    // Test simple queries
+    Test("utils", [](Fixture& f) {
+        // Resolve NULL and the empty string
+        ensure_equals(dbadb::parse_op_report(*f.db, NULL), (const char*)NULL);
+        ensure_equals(dbadb::parse_op_report(*f.db, ""), (const char*)NULL);
+
+        // Resolve from names
+        wassert(actual(dbadb::parse_op_report(*f.db, "synop")) == "synop");
+
+        // Resolve from nonexisting numbers
+        try {
+            dbadb::parse_op_report(*f.db, "11234");
+            ensure(false);
+        } catch (...) {}
+
+        // Resolve from nonexisting names
+        try {
+            dbadb::parse_op_report(*f.db, "lolcats");
+            ensure(false);
+        } catch (...) {}
+    }),
+    Test("import", [](Fixture& f) {
+        Dbadb dbadb(*f.db);
+
+        // Import a synop
+        cmdline::Reader reader;
+        ensure_equals(dbadb.do_import(dballe::tests::datafile("bufr/obs0-1.22.bufr"), reader), 0);
+
+        // Export forcing report as temp
+        Record query;
+        ArrayFile file(BUFR);
+        ensure_equals(dbadb.do_export(query, file, "generic", "ship"), 0);
+
+        ensure_equals(file.msgs.size(), 1u);
+
+        // Decode results
+        auto importer = msg::Importer::create(BUFR);
+        Msgs msgs;
+        importer->from_rawmsg(file.msgs[0], msgs);
+        ensure_equals(msgs.size(), 1u);
+
+        // Ensure they're ships
+        ensure_equals(msgs[0]->type, MSG_SHIP);
+
+        // Check 001194 [SIM] Report mnemonic(CCITTIA5), too
+        const Var* var = msgs[0]->get_rep_memo_var();
+        ensure(var != 0);
+        ensure(var->enqc() != 0);
+        ensure_equals(var->enq<std::string>(), "ship");
+    }),
+};
+
+test_group tg1("cmdline_dbadb_mem", nullptr, db::MEM, tests);
+test_group tg2("cmdline_dbadb_v6_sqlite", "SQLITE", db::V6, tests);
 #ifdef HAVE_ODBC
-tut::tg tests_dbadb_v5_tg("cmdline_dbadb_v5", db::V5);
-tut::tg tests_dbadb_v6_tg("cmdline_dbadb_v6", db::V6);
+test_group tg3("cmdline_dbadb_v5_odbc", "ODBC", db::V5, tests);
+test_group tg4("cmdline_dbadb_v6_odbc", "ODBC", db::V6, tests);
+#endif
+#ifdef HAVE_LIBPQ
+test_group tg6("cmdline_dbadb_v6_postgresql", "POSTGRESQL", db::V6, tests);
+#endif
+#ifdef HAVE_MYSQL
+test_group tg8("cmdline_dbadb_v6_mysql", "MYSQL", db::V6, tests);
 #endif
 
 }
-
