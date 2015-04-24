d158c2e2915b4c4951abc1c701e7e9c4532d81df
Enrico Zini <enrico@enricozini.org>
2015-04-24 10:52:19 +0000

    Ported summary queries to the new test_group
    git-svn-id: https://svn.smr.arpa.emr.it/dballe/dballe/trunk@4694 5c06e07b-eeef-0310-a3fe-9f68d50baefc

--- dballe/db/db-summary-query-tut.cc
+++ dballe/db/db-summary-query-tut.cc
@@ -31,14 +31,14 @@ using namespace std;
 
 namespace {
 
-struct db_tests_query : public db_test
+struct Fixture : public dballe::tests::DBFixture
 {
     dballe::tests::TestStation st1;
     dballe::tests::TestStation st2;
     int st1_id;
     int st2_id;
 
-    db_tests_query()
+    Fixture()
     {
         st1.lat = 12.34560;
         st1.lon = 76.54320;
@@ -53,6 +53,12 @@ struct db_tests_query : public db_test
         st2.info["metar"].set("B07030", 110.0); // height
         st2.info["metar"].set("block", 3);
         st2.info["metar"].set("station", 4);
+
+        insert_data();
+    }
+
+    void insert_data()
+    {
 
         dballe::tests::TestRecord rec1;
         rec1.station = st1;
@@ -88,208 +94,200 @@ struct db_tests_query : public db_test
         rec2.data.set(WR_VAR(0, 12, 103), 291.0);
         wruntest(rec2.insert, *db, true);
     }
+
+    void reset()
+    {
+        dballe::tests::DBFixture::reset();
+        insert_data();
+    }
 };
 
-}
+typedef dballe::tests::db_test_group<Fixture> test_group;
+typedef test_group::Test Test;
 
-namespace tut {
+std::vector<Test> tests {
+    Test("query_ana_id", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query(str::fmtf("ana_id=%d", f.st1_id), 2));
+        wassert(actual(db).try_summary_query(str::fmtf("ana_id=%d", f.st2_id), 2));
+        wassert(actual(db).try_summary_query(str::fmtf("ana_id=%d", (f.st1_id + f.st2_id) * 2), 0));
+    }),
+    Test("query_station_vars", [](Fixture& f) {
+        auto& db = *f.db;
+        Record query;
+        query.set_ana_context();
+        unique_ptr<db::Cursor> cur = db.query_summary(query);
+        ensure_equals(cur->test_iterate(), 8);
+    }),
+    Test("query_year", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("year=1001", 0));
+        wassert(actual(db).try_summary_query("yearmin=1999", 0));
+        auto check_base = [](WIBBLE_TEST_LOCPRM, const vector<Record>& res) {
+            wassert(actual(res[0][DBA_KEY_LAT].enqi()) == 1234560);
+            wassert(actual(res[0][DBA_KEY_LON].enqi()) == 7654320);
+            wassert(actual(res[0].get_level()) == Level(10, 11, 15, 22));
+            wassert(actual(res[0].get_trange()) == Trange(20, 111, 122));
+            wassert(actual(res[0][DBA_KEY_VAR].enqc()) == "B12101");
+        };
+        auto check_nodetails = [&](WIBBLE_TEST_LOCPRM, const vector<Record>& res) {
+            wruntest(check_base, res);
+            wassert(actual(res[0].contains(DBA_KEY_CONTEXT_ID)).isfalse());
+            wassert(actual(res[0].contains(DBA_KEY_YEARMIN)).isfalse());
+            wassert(actual(res[0].contains(DBA_KEY_YEARMAX)).isfalse());
+        };
+        auto check_details = [&](WIBBLE_TEST_LOCPRM, const vector<Record>& res) {
+            wruntest(check_base, res);
+            wassert(actual(res[0][DBA_KEY_CONTEXT_ID].enqi()) == 2);
+            wassert(actual(res[0].get_datetimemin()) == Datetime(1945, 4, 25, 8));
+            wassert(actual(res[0].get_datetimemax()) == Datetime(1945, 4, 26, 8));
+        };
+        wassert(actual(db).try_summary_query("yearmin=1945", 4, check_nodetails));
+        wassert(actual(db).try_summary_query("yearmin=1945, query=details", 4, check_details));
+        wassert(actual(db).try_summary_query("yearmax=1944", 0));
+        wassert(actual(db).try_summary_query("yearmax=1945", 4));
+        wassert(actual(db).try_summary_query("yearmax=2030", 4));
+        wassert(actual(db).try_summary_query("year=1944", 0));
+        wassert(actual(db).try_summary_query("year=1945", 4));
+        wassert(actual(db).try_summary_query("year=1946", 0));
+    }),
+    Test("query_blockstation", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("B01001=1", 2));
+        wassert(actual(db).try_summary_query("B01001=2", 0));
+        wassert(actual(db).try_summary_query("B01002=3", 0));
+        wassert(actual(db).try_summary_query("B01002=4", 2));
+    }),
+    Test("query_ana_filter", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("ana_filter=block=1", 2));
+        wassert(actual(db).try_summary_query("ana_filter=B01001=1", 2));
+        wassert(actual(db).try_summary_query("ana_filter=block>1", 2));
+        wassert(actual(db).try_summary_query("ana_filter=B01001>1", 2));
+        wassert(actual(db).try_summary_query("ana_filter=block<=1", 2));
+        wassert(actual(db).try_summary_query("ana_filter=B01001>3", 0));
+        wassert(actual(db).try_summary_query("ana_filter=B01001>=3", 2));
+        wassert(actual(db).try_summary_query("ana_filter=B01001<=1", 2));
+        wassert(actual(db).try_summary_query("ana_filter=0<=B01001<=2", 2));
+        wassert(actual(db).try_summary_query("ana_filter=1<=B01001<=1", 2));
+        wassert(actual(db).try_summary_query("ana_filter=2<=B01001<=4", 2));
+        wassert(actual(db).try_summary_query("ana_filter=4<=B01001<=6", 0));
+    }),
+    Test("query_data_filter", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("data_filter=B12101<300.0", 1));
+        wassert(actual(db).try_summary_query("data_filter=B12101<=300.0", 2));
+        wassert(actual(db).try_summary_query("data_filter=B12101=300.0", 1));
+        wassert(actual(db).try_summary_query("data_filter=B12101>=300,0", 1));
+        wassert(actual(db).try_summary_query("data_filter=B12101>300.0", 1));
+        wassert(actual(db).try_summary_query("data_filter=B12101<400.0", 2));
+        wassert(actual(db).try_summary_query("data_filter=B12101<=400.0", 2));
+        wassert(actual(db).try_summary_query("data_filter=B12102>400.0", 0));
+    }),
+    Test("query_lat_lon", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("latmin=11.0", 4));
+        wassert(actual(db).try_summary_query("latmin=12.34560", 4));
+        wassert(actual(db).try_summary_query("latmin=13.0", 2));
+        wassert(actual(db).try_summary_query("latmax=11.0", 0));
+        wassert(actual(db).try_summary_query("latmax=12.34560", 2));
+        wassert(actual(db).try_summary_query("latmax=13.0", 2));
+        wassert(actual(db).try_summary_query("lonmin=75., lonmax=77.", 2));
+        wassert(actual(db).try_summary_query("lonmin=76.54320, lonmax=76.54320", 2));
+        wassert(actual(db).try_summary_query("lonmin=76.54330, lonmax=77.", 0));
+        wassert(actual(db).try_summary_query("lonmin=77., lonmax=76.54310", 2));
+        wassert(actual(db).try_summary_query("lonmin=77., lonmax=76.54320", 4));
+        wassert(actual(db).try_summary_query("lonmin=77., lonmax=-10", 0));
+    }),
+    Test("query_mobile", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("mobile=0", 4));
+        wassert(actual(db).try_summary_query("mobile=1", 0));
+    }),
+    Test("query_ident", [](Fixture& f) {
+        auto& db = *f.db;
+        // TODO: add mobile stations to the fixture so we can query ident
+    }),
+    Test("query_timerange", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("pindicator=20", 4));
+        wassert(actual(db).try_summary_query("pindicator=21", 0));
+        wassert(actual(db).try_summary_query("p1=111", 4));
+        wassert(actual(db).try_summary_query("p1=112", 0));
+        wassert(actual(db).try_summary_query("p2=121", 0));
+        wassert(actual(db).try_summary_query("p2=122", 4));
+        wassert(actual(db).try_summary_query("p2=123", 0));
+    }),
+    Test("query_level", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("leveltype1=10", 4));
+        wassert(actual(db).try_summary_query("leveltype1=11", 0));
+        wassert(actual(db).try_summary_query("leveltype2=15", 4));
+        wassert(actual(db).try_summary_query("leveltype2=16", 0));
+        wassert(actual(db).try_summary_query("l1=11", 4));
+        wassert(actual(db).try_summary_query("l1=12", 0));
+        wassert(actual(db).try_summary_query("l2=22", 4));
+        wassert(actual(db).try_summary_query("l2=23", 0));
+    }),
+    Test("query_var", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("var=B01001", 0));
+        wassert(actual(db).try_summary_query("var=B12101", 2));
+        wassert(actual(db).try_summary_query("var=B12102", 0));
+    }),
+    Test("query_rep_memo", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("rep_memo=synop", 0));
+        wassert(actual(db).try_summary_query("rep_memo=metar", 4));
+        wassert(actual(db).try_summary_query("rep_memo=temp", 0));
+    }),
+    Test("query_priority", [](Fixture& f) {
+        auto& db = *f.db;
+        wassert(actual(db).try_summary_query("priority=101", 0));
+        wassert(actual(db).try_summary_query("priority=81", 4));
+        wassert(actual(db).try_summary_query("priority=102", 0));
+        wassert(actual(db).try_summary_query("priomin=70", 4));
+        wassert(actual(db).try_summary_query("priomin=80", 4));
+        wassert(actual(db).try_summary_query("priomin=90", 0));
+        wassert(actual(db).try_summary_query("priomax=70", 0));
+        wassert(actual(db).try_summary_query("priomax=81", 4));
+        wassert(actual(db).try_summary_query("priomax=100", 4));
+    }),
+    Test("query_context_id", [](Fixture& f) {
+        auto& db = *f.db;
+        // Collect a vector of valid context IDs
+        vector<int> context_ids;
+        // And an invalid one
+        int sum = 1;
+        Record query;
+        unique_ptr<db::Cursor> cur = db.query_data(query);
+        while (cur->next())
+        {
+            context_ids.push_back(cur->attr_reference_id());
+            sum += context_ids.back();
+        }
 
-using namespace dballe::tests;
-typedef db_tg<db_tests_query> tg;
-typedef tg::object to;
+        WIBBLE_TEST_INFO(info);
 
-template<> template<> void to::test<1>()
-{
-    wassert(actual(db).try_summary_query(str::fmtf("ana_id=%d", st1_id), 2));
-    wassert(actual(db).try_summary_query(str::fmtf("ana_id=%d", st2_id), 2));
-    wassert(actual(db).try_summary_query(str::fmtf("ana_id=%d", (st1_id + st2_id) * 2), 0));
-}
+        wassert(actual(db).try_summary_query(str::fmtf("context_id=%d", sum), 0));
+        for (vector<int>::const_iterator i = context_ids.begin(); i != context_ids.end(); ++i)
+        {
+            info() << "Context ID " << *i;
+            wassert(actual(db).try_summary_query(str::fmtf("context_id=%d", *i), 1));
+        }
+    }),
+};
 
-template<> template<> void to::test<2>()
-{
-    Record query;
-    query.set_ana_context();
-    unique_ptr<db::Cursor> cur = db->query_summary(query);
-    ensure_equals(cur->test_iterate(), 8);
-}
-
-template<> template<> void to::test<3>()
-{
-    wassert(actual(db).try_summary_query("year=1001", 0));
-    wassert(actual(db).try_summary_query("yearmin=1999", 0));
-    auto check_base = [](WIBBLE_TEST_LOCPRM, const vector<Record>& res) {
-        wassert(actual(res[0][DBA_KEY_LAT].enqi()) == 1234560);
-        wassert(actual(res[0][DBA_KEY_LON].enqi()) == 7654320);
-        wassert(actual(res[0].get_level()) == Level(10, 11, 15, 22));
-        wassert(actual(res[0].get_trange()) == Trange(20, 111, 122));
-        wassert(actual(res[0][DBA_KEY_VAR].enqc()) == "B12101");
-    };
-    auto check_nodetails = [&](WIBBLE_TEST_LOCPRM, const vector<Record>& res) {
-        wruntest(check_base, res);
-        wassert(actual(res[0].contains(DBA_KEY_CONTEXT_ID)).isfalse());
-        wassert(actual(res[0].contains(DBA_KEY_YEARMIN)).isfalse());
-        wassert(actual(res[0].contains(DBA_KEY_YEARMAX)).isfalse());
-    };
-    auto check_details = [&](WIBBLE_TEST_LOCPRM, const vector<Record>& res) {
-        wruntest(check_base, res);
-        wassert(actual(res[0][DBA_KEY_CONTEXT_ID].enqi()) == 2);
-        wassert(actual(res[0].get_datetimemin()) == Datetime(1945, 4, 25, 8));
-        wassert(actual(res[0].get_datetimemax()) == Datetime(1945, 4, 26, 8));
-    };
-    wassert(actual(db).try_summary_query("yearmin=1945", 4, check_nodetails));
-    wassert(actual(db).try_summary_query("yearmin=1945, query=details", 4, check_details));
-    wassert(actual(db).try_summary_query("yearmax=1944", 0));
-    wassert(actual(db).try_summary_query("yearmax=1945", 4));
-    wassert(actual(db).try_summary_query("yearmax=2030", 4));
-    wassert(actual(db).try_summary_query("year=1944", 0));
-    wassert(actual(db).try_summary_query("year=1945", 4));
-    wassert(actual(db).try_summary_query("year=1946", 0));
-}
-
-template<> template<> void to::test<4>()
-{
-    wassert(actual(db).try_summary_query("B01001=1", 2));
-    wassert(actual(db).try_summary_query("B01001=2", 0));
-    wassert(actual(db).try_summary_query("B01002=3", 0));
-    wassert(actual(db).try_summary_query("B01002=4", 2));
-}
-
-template<> template<> void to::test<5>()
-{
-    wassert(actual(db).try_summary_query("ana_filter=block=1", 2));
-    wassert(actual(db).try_summary_query("ana_filter=B01001=1", 2));
-    wassert(actual(db).try_summary_query("ana_filter=block>1", 2));
-    wassert(actual(db).try_summary_query("ana_filter=B01001>1", 2));
-    wassert(actual(db).try_summary_query("ana_filter=block<=1", 2));
-    wassert(actual(db).try_summary_query("ana_filter=B01001>3", 0));
-    wassert(actual(db).try_summary_query("ana_filter=B01001>=3", 2));
-    wassert(actual(db).try_summary_query("ana_filter=B01001<=1", 2));
-    wassert(actual(db).try_summary_query("ana_filter=0<=B01001<=2", 2));
-    wassert(actual(db).try_summary_query("ana_filter=1<=B01001<=1", 2));
-    wassert(actual(db).try_summary_query("ana_filter=2<=B01001<=4", 2));
-    wassert(actual(db).try_summary_query("ana_filter=4<=B01001<=6", 0));
-}
-
-template<> template<> void to::test<6>()
-{
-    wassert(actual(db).try_summary_query("data_filter=B12101<300.0", 1));
-    wassert(actual(db).try_summary_query("data_filter=B12101<=300.0", 2));
-    wassert(actual(db).try_summary_query("data_filter=B12101=300.0", 1));
-    wassert(actual(db).try_summary_query("data_filter=B12101>=300,0", 1));
-    wassert(actual(db).try_summary_query("data_filter=B12101>300.0", 1));
-    wassert(actual(db).try_summary_query("data_filter=B12101<400.0", 2));
-    wassert(actual(db).try_summary_query("data_filter=B12101<=400.0", 2));
-    wassert(actual(db).try_summary_query("data_filter=B12102>400.0", 0));
-}
-
-template<> template<> void to::test<7>()
-{
-    wassert(actual(db).try_summary_query("latmin=11.0", 4));
-    wassert(actual(db).try_summary_query("latmin=12.34560", 4));
-    wassert(actual(db).try_summary_query("latmin=13.0", 2));
-    wassert(actual(db).try_summary_query("latmax=11.0", 0));
-    wassert(actual(db).try_summary_query("latmax=12.34560", 2));
-    wassert(actual(db).try_summary_query("latmax=13.0", 2));
-    wassert(actual(db).try_summary_query("lonmin=75., lonmax=77.", 2));
-    wassert(actual(db).try_summary_query("lonmin=76.54320, lonmax=76.54320", 2));
-    wassert(actual(db).try_summary_query("lonmin=76.54330, lonmax=77.", 0));
-    wassert(actual(db).try_summary_query("lonmin=77., lonmax=76.54310", 2));
-    wassert(actual(db).try_summary_query("lonmin=77., lonmax=76.54320", 4));
-    wassert(actual(db).try_summary_query("lonmin=77., lonmax=-10", 0));
-}
-
-template<> template<> void to::test<8>()
-{
-    wassert(actual(db).try_summary_query("mobile=0", 4));
-    wassert(actual(db).try_summary_query("mobile=1", 0));
-}
-
-template<> template<> void to::test<9>()
-{
-    // TODO: add mobile stations to the fixture so we can query ident
-}
-
-template<> template<> void to::test<10>()
-{
-    wassert(actual(db).try_summary_query("pindicator=20", 4));
-    wassert(actual(db).try_summary_query("pindicator=21", 0));
-    wassert(actual(db).try_summary_query("p1=111", 4));
-    wassert(actual(db).try_summary_query("p1=112", 0));
-    wassert(actual(db).try_summary_query("p2=121", 0));
-    wassert(actual(db).try_summary_query("p2=122", 4));
-    wassert(actual(db).try_summary_query("p2=123", 0));
-}
-
-template<> template<> void to::test<11>()
-{
-    wassert(actual(db).try_summary_query("leveltype1=10", 4));
-    wassert(actual(db).try_summary_query("leveltype1=11", 0));
-    wassert(actual(db).try_summary_query("leveltype2=15", 4));
-    wassert(actual(db).try_summary_query("leveltype2=16", 0));
-    wassert(actual(db).try_summary_query("l1=11", 4));
-    wassert(actual(db).try_summary_query("l1=12", 0));
-    wassert(actual(db).try_summary_query("l2=22", 4));
-    wassert(actual(db).try_summary_query("l2=23", 0));
-}
-
-template<> template<> void to::test<12>()
-{
-    wassert(actual(db).try_summary_query("var=B01001", 0));
-    wassert(actual(db).try_summary_query("var=B12101", 2));
-    wassert(actual(db).try_summary_query("var=B12102", 0));
-}
-
-template<> template<> void to::test<13>()
-{
-    wassert(actual(db).try_summary_query("rep_memo=synop", 0));
-    wassert(actual(db).try_summary_query("rep_memo=metar", 4));
-    wassert(actual(db).try_summary_query("rep_memo=temp", 0));
-}
-
-template<> template<> void to::test<14>()
-{
-    wassert(actual(db).try_summary_query("priority=101", 0));
-    wassert(actual(db).try_summary_query("priority=81", 4));
-    wassert(actual(db).try_summary_query("priority=102", 0));
-    wassert(actual(db).try_summary_query("priomin=70", 4));
-    wassert(actual(db).try_summary_query("priomin=80", 4));
-    wassert(actual(db).try_summary_query("priomin=90", 0));
-    wassert(actual(db).try_summary_query("priomax=70", 0));
-    wassert(actual(db).try_summary_query("priomax=81", 4));
-    wassert(actual(db).try_summary_query("priomax=100", 4));
-}
-
-template<> template<> void to::test<15>()
-{
-    // Collect a vector of valid context IDs
-    vector<int> context_ids;
-    // And an invalid one
-    int sum = 1;
-    Record query;
-    unique_ptr<db::Cursor> cur = db->query_data(query);
-    while (cur->next())
-    {
-        context_ids.push_back(cur->attr_reference_id());
-        sum += context_ids.back();
-    }
-
-    WIBBLE_TEST_INFO(info);
-
-    wassert(actual(db).try_summary_query(str::fmtf("context_id=%d", sum), 0));
-    for (vector<int>::const_iterator i = context_ids.begin(); i != context_ids.end(); ++i)
-    {
-        info() << "Context ID " << *i;
-        wassert(actual(db).try_summary_query(str::fmtf("context_id=%d", *i), 1));
-    }
-}
+test_group tg1("db_summary_query_mem", nullptr, db::MEM, tests);
+test_group tg2("db_summary_query_v6_sqlite", "SQLITE", db::V6, tests);
+#ifdef HAVE_ODBC
+test_group tg4("db_summary_query_v6_odbc", "ODBC", db::V6, tests);
+#endif
+#ifdef HAVE_LIBPQ
+test_group tg6("db_summary_query_v6_postgresql", "POSTGRESQL", db::V6, tests);
+#endif
+#ifdef HAVE_MYSQL
+test_group tg8("db_summary_query_v6_mysql", "MYSQL", db::V6, tests);
+#endif
 
 }
-
-namespace {
-
-tut::tg db_tests_query_mem_tg("db_summary_query_mem", MEM);
-// tut::tg db_tests_query_v5_tg("db_summary_query_v5", V5); Summary queries are not implemented for V5 databases
-tut::tg db_tests_query_v6_tg("db_summary_query_v6", V6);
-
-}
-
