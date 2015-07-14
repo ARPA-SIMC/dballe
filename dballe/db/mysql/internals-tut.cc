53a8b714d32f77da9a997f6764df6107d14e7561
Enrico Zini <enrico@enricozini.org>
2015-07-14 10:45:34 +0200

    Ported tests to new test suite

--- dballe/db/mysql/internals-tut.cc
+++ dballe/db/mysql/internals-tut.cc
@@ -1,288 +1,240 @@
-/*
- * Copyright (C) 2015  ARPA-SIM <urpsim@smr.arpa.emr.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Author: Enrico Zini <enrico@enricozini.com>
- */
-#include "db/tests.h"
+#include "core/tests.h"
 #include "internals.h"
 
+using namespace std;
+using namespace wibble::tests;
 using namespace dballe;
 using namespace dballe::db;
 using namespace wreport;
-using namespace wibble::tests;
-using namespace std;
 
-namespace tut {
+namespace {
 
-struct db_mysql_internals_shar
+struct Fixture : dballe::tests::Fixture
 {
     MySQLConnection conn;
 
-    db_mysql_internals_shar()
+    Fixture()
     {
         conn.open_test();
     }
 
-    ~db_mysql_internals_shar()
-    {
-    }
-
     void reset()
     {
+        dballe::tests::Fixture::reset();
         conn.drop_table_if_exists("dballe_test");
         conn.exec_no_data("CREATE TABLE dballe_test (val INTEGER NOT NULL)");
     }
 };
-TESTGRP(db_mysql_internals);
 
-// Test parsing urls
-template<> template<>
-void to::test<1>()
-{
-    mysql::ConnectInfo info;
+typedef dballe::tests::test_group<Fixture> test_group;
+typedef test_group::Test Test;
 
-    info.parse_url("mysql:");
-    wassert(actual(info.host) == "");
-    wassert(actual(info.user) == "");
-    wassert(actual(info.has_passwd).isfalse());
-    wassert(actual(info.passwd) == "");
-    wassert(actual(info.has_dbname).isfalse());
-    wassert(actual(info.dbname) == "");
-    wassert(actual(info.port) == 0);
-    wassert(actual(info.unix_socket) == "");
+std::vector<Test> tests {
+    Test("parse_url", [](Fixture& f) {
+        // Test parsing urls
+        mysql::ConnectInfo info;
 
-    info.parse_url("mysql://");
-    wassert(actual(info.host) == "");
-    wassert(actual(info.user) == "");
-    wassert(actual(info.has_passwd).isfalse());
-    wassert(actual(info.passwd) == "");
-    wassert(actual(info.has_dbname).isfalse());
-    wassert(actual(info.dbname) == "");
-    wassert(actual(info.port) == 0);
-    wassert(actual(info.unix_socket) == "");
+        info.parse_url("mysql:");
+        wassert(actual(info.host) == "");
+        wassert(actual(info.user) == "");
+        wassert(actual(info.has_passwd).isfalse());
+        wassert(actual(info.passwd) == "");
+        wassert(actual(info.has_dbname).isfalse());
+        wassert(actual(info.dbname) == "");
+        wassert(actual(info.port) == 0);
+        wassert(actual(info.unix_socket) == "");
 
-    info.parse_url("mysql://localhost/");
-    wassert(actual(info.host) == "localhost");
-    wassert(actual(info.user) == "");
-    wassert(actual(info.has_passwd).isfalse());
-    wassert(actual(info.passwd) == "");
-    wassert(actual(info.has_dbname).isfalse());
-    wassert(actual(info.dbname) == "");
-    wassert(actual(info.port) == 0);
-    wassert(actual(info.unix_socket) == "");
+        info.parse_url("mysql://");
+        wassert(actual(info.host) == "");
+        wassert(actual(info.user) == "");
+        wassert(actual(info.has_passwd).isfalse());
+        wassert(actual(info.passwd) == "");
+        wassert(actual(info.has_dbname).isfalse());
+        wassert(actual(info.dbname) == "");
+        wassert(actual(info.port) == 0);
+        wassert(actual(info.unix_socket) == "");
 
-    info.parse_url("mysql://localhost:1234/");
-    wassert(actual(info.host) == "localhost");
-    wassert(actual(info.user) == "");
-    wassert(actual(info.has_passwd).isfalse());
-    wassert(actual(info.passwd) == "");
-    wassert(actual(info.has_dbname).isfalse());
-    wassert(actual(info.dbname) == "");
-    wassert(actual(info.port) == 1234);
-    wassert(actual(info.unix_socket) == "");
+        info.parse_url("mysql://localhost/");
+        wassert(actual(info.host) == "localhost");
+        wassert(actual(info.user) == "");
+        wassert(actual(info.has_passwd).isfalse());
+        wassert(actual(info.passwd) == "");
+        wassert(actual(info.has_dbname).isfalse());
+        wassert(actual(info.dbname) == "");
+        wassert(actual(info.port) == 0);
+        wassert(actual(info.unix_socket) == "");
 
-    info.parse_url("mysql://localhost:1234/?user=enrico");
-    wassert(actual(info.host) == "localhost");
-    wassert(actual(info.user) == "enrico");
-    wassert(actual(info.has_passwd).isfalse());
-    wassert(actual(info.passwd) == "");
-    wassert(actual(info.has_dbname).isfalse());
-    wassert(actual(info.dbname) == "");
-    wassert(actual(info.port) == 1234);
-    wassert(actual(info.unix_socket) == "");
+        info.parse_url("mysql://localhost:1234/");
+        wassert(actual(info.host) == "localhost");
+        wassert(actual(info.user) == "");
+        wassert(actual(info.has_passwd).isfalse());
+        wassert(actual(info.passwd) == "");
+        wassert(actual(info.has_dbname).isfalse());
+        wassert(actual(info.dbname) == "");
+        wassert(actual(info.port) == 1234);
+        wassert(actual(info.unix_socket) == "");
 
-    info.parse_url("mysql://localhost:1234/foo?user=enrico");
-    wassert(actual(info.host) == "localhost");
-    wassert(actual(info.user) == "enrico");
-    wassert(actual(info.has_passwd).isfalse());
-    wassert(actual(info.passwd) == "");
-    wassert(actual(info.has_dbname).istrue());
-    wassert(actual(info.dbname) == "foo");
-    wassert(actual(info.port) == 1234);
-    wassert(actual(info.unix_socket) == "");
+        info.parse_url("mysql://localhost:1234/?user=enrico");
+        wassert(actual(info.host) == "localhost");
+        wassert(actual(info.user) == "enrico");
+        wassert(actual(info.has_passwd).isfalse());
+        wassert(actual(info.passwd) == "");
+        wassert(actual(info.has_dbname).isfalse());
+        wassert(actual(info.dbname) == "");
+        wassert(actual(info.port) == 1234);
+        wassert(actual(info.unix_socket) == "");
 
-    info.parse_url("mysql://localhost:1234/foo?user=enrico&password=secret");
-    wassert(actual(info.host) == "localhost");
-    wassert(actual(info.user) == "enrico");
-    wassert(actual(info.has_passwd).istrue());
-    wassert(actual(info.passwd) == "secret");
-    wassert(actual(info.has_dbname).istrue());
-    wassert(actual(info.dbname) == "foo");
-    wassert(actual(info.port) == 1234);
-    wassert(actual(info.unix_socket) == "");
+        info.parse_url("mysql://localhost:1234/foo?user=enrico");
+        wassert(actual(info.host) == "localhost");
+        wassert(actual(info.user) == "enrico");
+        wassert(actual(info.has_passwd).isfalse());
+        wassert(actual(info.passwd) == "");
+        wassert(actual(info.has_dbname).istrue());
+        wassert(actual(info.dbname) == "foo");
+        wassert(actual(info.port) == 1234);
+        wassert(actual(info.unix_socket) == "");
 
-    info.parse_url("mysql://localhost/foo?user=enrico&password=secret");
-    wassert(actual(info.host) == "localhost");
-    wassert(actual(info.user) == "enrico");
-    wassert(actual(info.has_passwd).istrue());
-    wassert(actual(info.passwd) == "secret");
-    wassert(actual(info.has_dbname).istrue());
-    wassert(actual(info.dbname) == "foo");
-    wassert(actual(info.port) == 0);
-    wassert(actual(info.unix_socket) == "");
+        info.parse_url("mysql://localhost:1234/foo?user=enrico&password=secret");
+        wassert(actual(info.host) == "localhost");
+        wassert(actual(info.user) == "enrico");
+        wassert(actual(info.has_passwd).istrue());
+        wassert(actual(info.passwd) == "secret");
+        wassert(actual(info.has_dbname).istrue());
+        wassert(actual(info.dbname) == "foo");
+        wassert(actual(info.port) == 1234);
+        wassert(actual(info.unix_socket) == "");
 
-    info.parse_url("mysql:///foo?user=enrico&password=secret");
-    wassert(actual(info.host) == "");
-    wassert(actual(info.user) == "enrico");
-    wassert(actual(info.has_passwd).istrue());
-    wassert(actual(info.passwd) == "secret");
-    wassert(actual(info.has_dbname).istrue());
-    wassert(actual(info.dbname) == "foo");
-    wassert(actual(info.port) == 0);
-    wassert(actual(info.unix_socket) == "");
-}
+        info.parse_url("mysql://localhost/foo?user=enrico&password=secret");
+        wassert(actual(info.host) == "localhost");
+        wassert(actual(info.user) == "enrico");
+        wassert(actual(info.has_passwd).istrue());
+        wassert(actual(info.passwd) == "secret");
+        wassert(actual(info.has_dbname).istrue());
+        wassert(actual(info.dbname) == "foo");
+        wassert(actual(info.port) == 0);
+        wassert(actual(info.unix_socket) == "");
 
-// Test querying int values
-template<> template<>
-void to::test<2>()
-{
-    using namespace mysql;
+        info.parse_url("mysql:///foo?user=enrico&password=secret");
+        wassert(actual(info.host) == "");
+        wassert(actual(info.user) == "enrico");
+        wassert(actual(info.has_passwd).istrue());
+        wassert(actual(info.passwd) == "secret");
+        wassert(actual(info.has_dbname).istrue());
+        wassert(actual(info.dbname) == "foo");
+        wassert(actual(info.port) == 0);
+        wassert(actual(info.unix_socket) == "");
+    }),
+    Test("query_int", [](Fixture& f) {
+        // Test querying int values
+        using namespace mysql;
 
-    reset();
+        f.conn.exec_no_data("INSERT INTO dballe_test VALUES (1)");
+        f.conn.exec_no_data("INSERT INTO dballe_test VALUES (2)");
 
-    conn.exec_no_data("INSERT INTO dballe_test VALUES (1)");
-    conn.exec_no_data("INSERT INTO dballe_test VALUES (2)");
+        auto res = f.conn.exec_store("SELECT val FROM dballe_test");
 
-    auto res = conn.exec_store("SELECT val FROM dballe_test");
-
-    int val = 0;
-    unsigned count = 0;
-    while (Row row = res.fetch())
-    {
-        val += row.as_int(0);
-        ++count;
-    }
-    wassert(actual(count) == 2);
-    wassert(actual(val) == 3);
-}
-
-// Test querying int values, with potential NULLs
-template<> template<>
-void to::test<3>()
-{
-    reset();
-
-    conn.drop_table_if_exists("dballe_testnull");
-    conn.exec_no_data("CREATE TABLE dballe_testnull (val INTEGER)");
-    conn.exec_no_data("INSERT INTO dballe_testnull VALUES (NULL)");
-    conn.exec_no_data("INSERT INTO dballe_testnull VALUES (42)");
-
-    auto res = conn.exec_store("SELECT val FROM dballe_testnull");
-    wassert(actual(res.rowcount()) == 2);
-
-    int val = 0;
-    unsigned count = 0;
-    unsigned countnulls = 0;
-    while (auto row = res.fetch())
-    {
-        if (row.isnull(0))
-            ++countnulls;
-        else
+        int val = 0;
+        unsigned count = 0;
+        while (Row row = res.fetch())
+        {
             val += row.as_int(0);
-        ++count;
-    }
+            ++count;
+        }
+        wassert(actual(count) == 2);
+        wassert(actual(val) == 3);
+    }),
+    Test("query_int_null", [](Fixture& f) {
+        // Test querying int values, with potential NULLs
+        f.conn.drop_table_if_exists("dballe_testnull");
+        f.conn.exec_no_data("CREATE TABLE dballe_testnull (val INTEGER)");
+        f.conn.exec_no_data("INSERT INTO dballe_testnull VALUES (NULL)");
+        f.conn.exec_no_data("INSERT INTO dballe_testnull VALUES (42)");
 
-    wassert(actual(val) == 42);
-    wassert(actual(count) == 2);
-    wassert(actual(countnulls) == 1);
-}
+        auto res = f.conn.exec_store("SELECT val FROM dballe_testnull");
+        wassert(actual(res.rowcount()) == 2);
 
-// Test querying unsigned values
-template<> template<>
-void to::test<4>()
-{
-    reset();
+        int val = 0;
+        unsigned count = 0;
+        unsigned countnulls = 0;
+        while (auto row = res.fetch())
+        {
+            if (row.isnull(0))
+                ++countnulls;
+            else
+                val += row.as_int(0);
+            ++count;
+        }
 
-    conn.drop_table_if_exists("dballe_testbig");
-    conn.exec_no_data("CREATE TABLE dballe_testbig (val BIGINT)");
-    conn.exec_no_data("INSERT INTO dballe_testbig VALUES (0xFFFFFFFE)");
+        wassert(actual(val) == 42);
+        wassert(actual(count) == 2);
+        wassert(actual(countnulls) == 1);
+    }),
+    Test("query_unsigned", [](Fixture& f) {
+        // Test querying unsigned values
+        f.conn.drop_table_if_exists("dballe_testbig");
+        f.conn.exec_no_data("CREATE TABLE dballe_testbig (val BIGINT)");
+        f.conn.exec_no_data("INSERT INTO dballe_testbig VALUES (0xFFFFFFFE)");
 
-    auto res = conn.exec_store("SELECT val FROM dballe_testbig");
-    wassert(actual(res.rowcount()) == 1);
+        auto res = f.conn.exec_store("SELECT val FROM dballe_testbig");
+        wassert(actual(res.rowcount()) == 1);
 
-    unsigned val = 0;
-    unsigned count = 0;
-    while (auto row = res.fetch())
-    {
-        val += row.as_unsigned(0);
-        ++count;
-    }
-    wassert(actual(val) == 0xFFFFFFFE);
-    wassert(actual(count) == 1);
-}
+        unsigned val = 0;
+        unsigned count = 0;
+        while (auto row = res.fetch())
+        {
+            val += row.as_unsigned(0);
+            ++count;
+        }
+        wassert(actual(val) == 0xFFFFFFFE);
+        wassert(actual(count) == 1);
+    }),
+    Test("query_unsigned_short", [](Fixture& f) {
+        // Test querying unsigned short values
+        char buf[200];
+        snprintf(buf, 200, "INSERT INTO dballe_test VALUES (%d)", (int)WR_VAR(3, 1, 12));
+        f.conn.exec_no_data(buf);
 
-// Test querying unsigned short values
-template<> template<>
-void to::test<5>()
-{
-    reset();
+        auto res = f.conn.exec_store("SELECT val FROM dballe_test");
+        wassert(actual(res.rowcount()) == 1);
 
-    char buf[200];
-    snprintf(buf, 200, "INSERT INTO dballe_test VALUES (%d)", (int)WR_VAR(3, 1, 12));
-    conn.exec_no_data(buf);
+        Varcode val = 0;
+        unsigned count = 0;
+        while (auto row = res.fetch())
+        {
+            val = (Varcode)row.as_int(0);
+            ++count;
+        }
+        wassert(actual(count) == 1);
+        wassert(actual(val) == WR_VAR(3, 1, 12));
+    }),
+    Test("has_tables", [](Fixture& f) {
+        // Test has_tables
+        wassert(actual(f.conn.has_table("this_should_not_exist")).isfalse());
+        wassert(actual(f.conn.has_table("dballe_test")).istrue());
+    }),
+    Test("settings", [](Fixture& f) {
+        // Test settings
+        f.conn.drop_table_if_exists("dballe_settings");
+        wassert(actual(f.conn.has_table("dballe_settings")).isfalse());
 
-    auto res = conn.exec_store("SELECT val FROM dballe_test");
-    wassert(actual(res.rowcount()) == 1);
+        wassert(actual(f.conn.get_setting("test_key")) == "");
 
-    Varcode val = 0;
-    unsigned count = 0;
-    while (auto row = res.fetch())
-    {
-        val = (Varcode)row.as_int(0);
-        ++count;
-    }
-    wassert(actual(count) == 1);
-    wassert(actual(val) == WR_VAR(3, 1, 12));
-}
+        f.conn.set_setting("test_key", "42");
+        wassert(actual(f.conn.has_table("dballe_settings")).istrue());
 
-// Test has_tables
-template<> template<>
-void to::test<6>()
-{
-    reset();
+        wassert(actual(f.conn.get_setting("test_key")) == "42");
+    }),
+    Test("auto_increment", [](Fixture& f) {
+        // Test auto_increment
+        f.conn.drop_table_if_exists("dballe_testai");
+        f.conn.exec_no_data("CREATE TABLE dballe_testai (id INTEGER AUTO_INCREMENT PRIMARY KEY, val INTEGER)");
+        f.conn.exec_no_data("INSERT INTO dballe_testai (val) VALUES (42)");
+        wassert(actual(f.conn.get_last_insert_id()) == 1);
+        f.conn.exec_no_data("INSERT INTO dballe_testai (val) VALUES (43)");
+        wassert(actual(f.conn.get_last_insert_id()) == 2);
+    }),
+};
 
-    wassert(actual(conn.has_table("this_should_not_exist")).isfalse());
-    wassert(actual(conn.has_table("dballe_test")).istrue());
-}
-
-// Test settings
-template<> template<>
-void to::test<7>()
-{
-    conn.drop_table_if_exists("dballe_settings");
-    wassert(actual(conn.has_table("dballe_settings")).isfalse());
-
-    wassert(actual(conn.get_setting("test_key")) == "");
-
-    conn.set_setting("test_key", "42");
-    wassert(actual(conn.has_table("dballe_settings")).istrue());
-
-    wassert(actual(conn.get_setting("test_key")) == "42");
-}
-
-// Test auto_increment
-template<> template<>
-void to::test<8>()
-{
-    conn.drop_table_if_exists("dballe_testai");
-    conn.exec_no_data("CREATE TABLE dballe_testai (id INTEGER AUTO_INCREMENT PRIMARY KEY, val INTEGER)");
-    conn.exec_no_data("INSERT INTO dballe_testai (val) VALUES (42)");
-    wassert(actual(conn.get_last_insert_id()) == 1);
-    conn.exec_no_data("INSERT INTO dballe_testai (val) VALUES (43)");
-    wassert(actual(conn.get_last_insert_id()) == 2);
-}
+test_group newtg("db_internals_mysql", tests);
 
 }
--- dballe/db/sqlite/internals-tut.cc
+++ dballe/db/sqlite/internals-tut.cc
@@ -1,178 +1,134 @@
-/*
- * Copyright (C) 2014  ARPA-SIM <urpsim@smr.arpa.emr.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Author: Enrico Zini <enrico@enricozini.com>
- */
-#include "db/tests.h"
+#include "core/tests.h"
 #include "internals.h"
 
+using namespace std;
+using namespace wibble::tests;
 using namespace dballe;
 using namespace dballe::db;
 using namespace wreport;
-using namespace wibble::tests;
-using namespace std;
 
-namespace tut {
+namespace {
 
-struct db_sqlite_internals_shar
+struct Fixture : dballe::tests::Fixture
 {
     SQLiteConnection conn;
 
-    db_sqlite_internals_shar()
+    Fixture()
     {
         conn.open_memory();
-    }
-
-    ~db_sqlite_internals_shar()
-    {
+        reset();
     }
 
     void reset()
     {
+        dballe::tests::Fixture::reset();
         conn.drop_table_if_exists("dballe_test");
         conn.exec("CREATE TABLE dballe_test (val INTEGER NOT NULL)");
     }
 };
-TESTGRP(db_sqlite_internals);
 
-// Test querying int values
-template<> template<>
-void to::test<1>()
-{
-    reset();
+typedef dballe::tests::test_group<Fixture> test_group;
+typedef test_group::Test Test;
 
-    conn.exec("INSERT INTO dballe_test VALUES (1)");
-    conn.exec("INSERT INTO dballe_test VALUES (2)");
+std::vector<Test> tests {
+    Test("query_int", [](Fixture& f) {
+        // Test querying int values
+        f.conn.exec("INSERT INTO dballe_test VALUES (1)");
+        f.conn.exec("INSERT INTO dballe_test VALUES (2)");
 
-    auto s = conn.sqlitestatement("SELECT val FROM dballe_test");
+        auto s = f.conn.sqlitestatement("SELECT val FROM dballe_test");
 
-    int val = 0;
-    unsigned count = 0;
-    s->execute([&]() {
-        val += s->column_int(0);
-        ++count;
-    });
-    wassert(actual(count) == 2);
-    wassert(actual(val) == 3);
-}
-
-// Test querying int values, with potential NULLs
-template<> template<>
-void to::test<2>()
-{
-    reset();
-
-    conn.exec("CREATE TABLE dballe_testnull (val INTEGER)");
-    conn.exec("INSERT INTO dballe_testnull VALUES (NULL)");
-    conn.exec("INSERT INTO dballe_testnull VALUES (42)");
-
-    auto s = conn.sqlitestatement("SELECT val FROM dballe_testnull");
-
-    int val = 0;
-    unsigned count = 0;
-    unsigned countnulls = 0;
-    s->execute([&]() {
-        if (s->column_isnull(0))
-            ++countnulls;
-        else
+        int val = 0;
+        unsigned count = 0;
+        s->execute([&]() {
             val += s->column_int(0);
-        ++count;
-    });
+            ++count;
+        });
+        wassert(actual(count) == 2);
+        wassert(actual(val) == 3);
+    }),
+    Test("query_int_null", [](Fixture& f) {
+        // Test querying int values, with potential NULLs
+        f.conn.exec("CREATE TABLE dballe_testnull (val INTEGER)");
+        f.conn.exec("INSERT INTO dballe_testnull VALUES (NULL)");
+        f.conn.exec("INSERT INTO dballe_testnull VALUES (42)");
 
-    wassert(actual(val) == 42);
-    wassert(actual(count) == 2);
-    wassert(actual(countnulls) == 1);
-}
+        auto s = f.conn.sqlitestatement("SELECT val FROM dballe_testnull");
 
-// Test querying unsigned values
-template<> template<>
-void to::test<3>()
-{
-    reset();
+        int val = 0;
+        unsigned count = 0;
+        unsigned countnulls = 0;
+        s->execute([&]() {
+            if (s->column_isnull(0))
+                ++countnulls;
+            else
+                val += s->column_int(0);
+            ++count;
+        });
 
-    conn.exec("INSERT INTO dballe_test VALUES (0xFFFFFFFE)");
+        wassert(actual(val) == 42);
+        wassert(actual(count) == 2);
+        wassert(actual(countnulls) == 1);
+    }),
+    Test("query_unsigned", [](Fixture& f) {
+        // Test querying unsigned values
+        f.conn.exec("INSERT INTO dballe_test VALUES (0xFFFFFFFE)");
 
-    auto s = conn.sqlitestatement("SELECT val FROM dballe_test");
+        auto s = f.conn.sqlitestatement("SELECT val FROM dballe_test");
 
-    unsigned val = 0;
-    unsigned count = 0;
-    s->execute([&]() {
-        val += s->column_int64(0);
-        ++count;
-    });
-    wassert(actual(count) == 1);
-    wassert(actual(val) == 0xFFFFFFFE);
-}
+        unsigned val = 0;
+        unsigned count = 0;
+        s->execute([&]() {
+            val += s->column_int64(0);
+            ++count;
+        });
+        wassert(actual(count) == 1);
+        wassert(actual(val) == 0xFFFFFFFE);
+    }),
+    Test("query_unsigned_short", [](Fixture& f) {
+        // Test querying unsigned short values
+        char buf[200];
+        snprintf(buf, 200, "INSERT INTO dballe_test VALUES (%d)", (int)WR_VAR(3, 1, 12));
+        f.conn.exec(buf);
 
-// Test querying unsigned short values
-template<> template<>
-void to::test<5>()
-{
-    reset();
+        auto s = f.conn.sqlitestatement("SELECT val FROM dballe_test");
 
-    char buf[200];
-    snprintf(buf, 200, "INSERT INTO dballe_test VALUES (%d)", (int)WR_VAR(3, 1, 12));
-    conn.exec(buf);
+        Varcode val = 0;
+        unsigned count = 0;
+        s->execute([&]() {
+            val = (Varcode)s->column_int(0);
+            ++count;
+        });
+        wassert(actual(count) == 1);
+        wassert(actual(val) == WR_VAR(3, 1, 12));
+    }),
+    Test("query_has_tables", [](Fixture& f) {
+        // Test has_tables
+        wassert(actual(f.conn.has_table("this_should_not_exist")).isfalse());
+        wassert(actual(f.conn.has_table("dballe_test")).istrue());
+    }),
+    Test("query_settings", [](Fixture& f) {
+        // Test settings
+        f.conn.drop_table_if_exists("dballe_settings");
+        wassert(actual(f.conn.has_table("dballe_settings")).isfalse());
 
-    auto s = conn.sqlitestatement("SELECT val FROM dballe_test");
+        wassert(actual(f.conn.get_setting("test_key")) == "");
 
-    Varcode val = 0;
-    unsigned count = 0;
-    s->execute([&]() {
-        val = (Varcode)s->column_int(0);
-        ++count;
-    });
-    wassert(actual(count) == 1);
-    wassert(actual(val) == WR_VAR(3, 1, 12));
-}
+        f.conn.set_setting("test_key", "42");
+        wassert(actual(f.conn.has_table("dballe_settings")).istrue());
 
-// Test has_tables
-template<> template<>
-void to::test<6>()
-{
-    reset();
+        wassert(actual(f.conn.get_setting("test_key")) == "42");
+    }),
+    Test("auto_increment", [](Fixture& f) {
+        // Test auto_increment
+        f.conn.exec("CREATE TABLE dballe_testai (id INTEGER PRIMARY KEY, val INTEGER)");
+        f.conn.exec("INSERT INTO dballe_testai (val) VALUES (42)");
+        wassert(actual(f.conn.get_last_insert_id()) == 1);
+        f.conn.exec("INSERT INTO dballe_testai (val) VALUES (43)");
+        wassert(actual(f.conn.get_last_insert_id()) == 2);
+    }),
+};
 
-    wassert(actual(conn.has_table("this_should_not_exist")).isfalse());
-    wassert(actual(conn.has_table("dballe_test")).istrue());
-}
-
-// Test settings
-template<> template<>
-void to::test<7>()
-{
-    conn.drop_table_if_exists("dballe_settings");
-    wassert(actual(conn.has_table("dballe_settings")).isfalse());
-
-    wassert(actual(conn.get_setting("test_key")) == "");
-
-    conn.set_setting("test_key", "42");
-    wassert(actual(conn.has_table("dballe_settings")).istrue());
-
-    wassert(actual(conn.get_setting("test_key")) == "42");
-}
-
-// Test auto_increment
-template<> template<>
-void to::test<8>()
-{
-    conn.exec("CREATE TABLE dballe_testai (id INTEGER PRIMARY KEY, val INTEGER)");
-    conn.exec("INSERT INTO dballe_testai (val) VALUES (42)");
-    wassert(actual(conn.get_last_insert_id()) == 1);
-    conn.exec("INSERT INTO dballe_testai (val) VALUES (43)");
-    wassert(actual(conn.get_last_insert_id()) == 2);
-}
+test_group newtg("db_internals_sqlite", tests);
 
 }
