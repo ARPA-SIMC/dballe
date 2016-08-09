665b3b7a26923d7f5b26035a549cc034b1176af5
Enrico Zini <enrico@enricozini.org>
2016-08-09 16:03:17 +0200

    Removed unneeded comments

--- dballe/msg/wr_importers/base.cc
+++ dballe/msg/wr_importers/base.cc
@@ -1,24 +1,3 @@
-/*
- * dballe/wr_importers/base - Base infrastructure for wreport importers
- *
- * Copyright (C) 2005--2015  ARPA-SIM <urpsim@smr.arpa.emr.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Author: Enrico Zini <enrico@enricozini.com>
- */
-
 #include "base.h"
 #include "core/var.h"
 #include <wreport/bulletin.h>
@@ -709,8 +688,6 @@ void SynopBaseImporter::import_var(const Var& var)
     }
 }
 
-} // namespace wr
-} // namespace msg
-} // namespace dballe
-
-/* vim:set ts=4 sw=4: */
+}
+}
+}
--- dballe/msg/wr_importers/synop.cc
+++ dballe/msg/wr_importers/synop.cc
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2005--2013  ARPA-SIM <urpsim@smr.arpa.emr.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Author: Enrico Zini <enrico@enricozini.com>
- */
-
 #include "base.h"
 #include <wreport/bulletin.h>
 #include <wreport/subset.h>
@@ -112,8 +93,6 @@ std::unique_ptr<Importer> Importer::createSynop(const msg::Importer::Options& op
 }
 
 
-} // namespace wbimporter
-} // namespace msg
-} // namespace dballe
-
-/* vim:set ts=4 sw=4: */
+}
+}
+}
