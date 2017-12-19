4e69f45a953c9123b99d8743f31479b140e06003
Emanuele Di Giacomo <edigiacomo@arpae.it>
2017-12-19 17:53:51 +0100

    Fix travis-build tag

--- .travis-build.sh
+++ .travis-build.sh
@@ -29,7 +29,7 @@ $builddep -y fedora/SPECS/dballe.spec
 
 if [[ $image =~ ^fedora: || $image =~ ^centos: ]]
 then
-    pkgname="$(rpmspec -q --qf="dballe-%{version}-%{release}\n" fedora/SPECS/dballe.spec | head -n1)"
+    pkgname=dballe-$(git describe --abbrev=0 --tags --match='v*' | sed -e 's,^v,,g')
     mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
     cp fedora/SPECS/dballe.spec ~/rpmbuild/SPECS/dballe.spec
     git archive --prefix=$pkgname/ --format=tar HEAD | gzip -c > ~/rpmbuild/SOURCES/$pkgname.tar.gz
