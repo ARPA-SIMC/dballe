7f211a1c4463d545e54d957a8b7c1ea3141a1f21
Enrico Zini <enrico@enricozini.org>
2018-06-01 21:40:20 +0200

    Use new colorful test runner

--- dballe/tests-main.cc
+++ dballe/tests-main.cc
@@ -1,4 +1,6 @@
 #include <wreport/utils/tests.h>
+#include <wreport/utils/testrunner.h>
+#include <wreport/utils/term.h>
 #include <dballe/db/tests.h>
 #include <signal.h>
 #include <cstdlib>
@@ -19,74 +21,27 @@ int main(int argc,const char* argv[])
 
     auto& tests = TestRegistry::get();
 
-    SimpleTestController controller;
+    wreport::term::Terminal output(stderr);
+
+    std::unique_ptr<FilteringTestController> controller;
+
+    bool verbose = (bool)getenv("TEST_VERBOSE");
+
+    if (verbose)
+        controller.reset(new VerboseTestController(output));
+    else
+        controller.reset(new SimpleTestController(output));
 
     if (const char* whitelist = getenv("TEST_WHITELIST"))
-        controller.whitelist = whitelist;
+        controller->whitelist = whitelist;
 
     if (const char* blacklist = getenv("TEST_BLACKLIST"))
-        controller.blacklist = blacklist;
+        controller->blacklist = blacklist;
 
-    auto all_results = tests.run_tests(controller);
-
-    unsigned methods_ok = 0;
-    unsigned methods_failed = 0;
-    unsigned methods_skipped = 0;
-    unsigned test_cases_ok = 0;
-    unsigned test_cases_failed = 0;
-
-    for (const auto& tc_res: all_results)
-    {
-        if (!tc_res.fail_setup.empty())
-        {
-            fprintf(stderr, "%s: %s\n", tc_res.test_case.c_str(), tc_res.fail_setup.c_str());
-            ++test_cases_failed;
-        } else {
-            if (!tc_res.fail_teardown.empty())
-            {
-                fprintf(stderr, "%s: %s\n", tc_res.test_case.c_str(), tc_res.fail_teardown.c_str());
-                ++test_cases_failed;
-            }
-            else
-                ++test_cases_ok;
-
-            for (const auto& tm_res: tc_res.methods)
-            {
-                if (tm_res.skipped)
-                    ++methods_skipped;
-                else if (tm_res.is_success())
-                    ++methods_ok;
-                else
-                {
-                    fprintf(stderr, "\n");
-                    if (tm_res.exception_typeid.empty())
-                        fprintf(stderr, "%s.%s: %s\n", tm_res.test_case.c_str(), tm_res.test_method.c_str(), tm_res.error_message.c_str());
-                    else
-                        fprintf(stderr, "%s.%s:[%s] %s\n", tm_res.test_case.c_str(), tm_res.test_method.c_str(), tm_res.exception_typeid.c_str(), tm_res.error_message.c_str());
-                    for (const auto& frame : tm_res.error_stack)
-                        fprintf(stderr, "  %s", frame.format().c_str());
-                    ++methods_failed;
-                }
-            }
-        }
-    }
-
-    bool success = true;
-
-    if (test_cases_failed)
-    {
-        success = false;
-        fprintf(stderr, "\n%u/%u test cases had issues initializing or cleaning up\n",
-                test_cases_failed, test_cases_ok + test_cases_failed);
-    }
-
-    if (methods_failed)
-    {
-        success = false;
-        fprintf(stderr, "\n%u/%u tests failed\n", methods_failed, methods_ok + methods_failed);
-    }
-    else
-        fprintf(stderr, "%u tests succeeded\n", methods_ok);
-
-    return success ? 0 : 1;
+    auto all_results = tests.run_tests(*controller);
+    TestResultStats rstats(all_results);
+    rstats.print_results(output);
+    if (verbose) rstats.print_stats(output);
+    rstats.print_summary(output);
+    return rstats.success ? 0 : 1;
 }
