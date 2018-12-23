4dd39700f8e9d01e5f2c0c21f8f1881b137f89fb
Enrico Zini <enrico@enricozini.org>
2018-12-23 15:37:48 +0100

    Do not test MEM format from python, since it's the same as a v7 in memory

--- python/test-volnd.py
+++ python/test-volnd.py
@@ -347,10 +347,6 @@ class TestReadV7(ReadMixin, unittest.TestCase):
     DB_FORMAT = "V7"
 
 
-class TestReadMEM(ReadMixin, unittest.TestCase):
-    DB_FORMAT = "MEM"
-
-
 if __name__ == "__main__":
     from testlib import main
     main("test-volnd")
