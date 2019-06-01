6e5b47bd4de8363573d12d491f5d9c73b06e4ca5
Enrico Zini <enrico@enricozini.org>
2019-06-01 15:55:52 +0200

    Added test case for issue158. refs: #158

--- python/test-db.py
+++ python/test-db.py
@@ -603,6 +603,34 @@ class CommonDBTestMixin(DballeDBMixin):
                             "B01012": 500,
                         })
 
+    def test_issue158(self):
+        with self.transaction() as tr:
+            tr.insert_data({
+                "lon": 1212345,
+                "lat": 4312345,
+                "rep_memo": "test1",
+                "datetime": datetime.datetime.now(),
+                "level": (103, 2000),
+                "trange": (254, 0, 0),
+                "B12101": 0
+            }, can_add_stations=True)
+
+            for row in tr.query_data():
+                data = row.data
+                self.assertEqual(data["report"], "test1")
+                self.assertEqual(data["rep_memo"], "test1")
+                data["rep_memo"] = "test2"
+                self.assertEqual(data["report"], "test2")
+                self.assertEqual(data["rep_memo"], "test2")
+                tr.insert_data(data, can_add_stations=True)
+
+        with self.transaction() as tr:
+            reports = []
+            for row in tr.query_data():
+                reports.append(row["report"])
+
+        self.assertEqual(reports, ["test1", "test2"])
+
 
 class FullDBTestMixin(CommonDBTestMixin):
     def test_transaction_enter_exit(self):
