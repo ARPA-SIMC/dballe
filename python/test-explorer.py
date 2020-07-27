87808e8523be6f1baab07c8ae13c8ff441bb421f
Enrico Zini <enrico@enricozini.org>
2020-07-27 16:55:51 +0200

    Reorganised explorer tests to test both json and xapian backends. refs: #216

--- python/test-explorer.py
+++ python/test-explorer.py
@@ -30,6 +30,28 @@ class BaseExplorerTestMixin(DballeDBMixin):
                     B01012=500)
             tr.insert_data(data, False, True)
 
+        # Track explorers created during the test case
+        self.explorers = set()
+
+    def tearDown(self):
+        # Cleanup persistent explorers created during the test case
+        for name in self.explorers:
+            self._cleanup(name)
+
+    def _cleanup(self, name):
+        if os.path.isdir(name):
+            shutil.rmtree(name)
+        elif os.path.exists(name):
+            os.unlink(name)
+
+    def _explorer(self, name=None, *args, **kw):
+        if name is None:
+            name = self.DEFAULT_EXPLORER_NAME
+        if name not in self.explorers:
+            self._cleanup(name)
+        self.explorers.add(name)
+        return self._make_explorer(name, *args, **kw)
+
     def assertExplorerContents(self, explorer, count_unfiltered=3, count_filtered=1):
         self.assertCountEqual(explorer.all_stations, [
             self._station("synop", 1, 12.34560, 76.54320, None),
@@ -52,7 +74,7 @@ class BaseExplorerTestMixin(DballeDBMixin):
             datetime.datetime(1945, 4, 25, 12, 0), datetime.datetime(1945, 4, 25, 12, 0), count_filtered)))
 
     def test_create(self):
-        explorer = self._explorer()
+        explorer = self._explorer("e1")
         with explorer.rebuild() as update:
             with self.db.transaction() as tr:
                 update.add_db(tr)
@@ -64,19 +86,19 @@ class BaseExplorerTestMixin(DballeDBMixin):
         json_string = explorer.to_json()
         self.assertIn('{"summary":{', json_string)
 
-        explorer1 = self._explorer()
+        explorer1 = self._explorer("e2")
         with explorer1.update() as update:
             update.add_json(json_string)
         explorer1.set_filter({"rep_memo": "amdar"})
         self.assertExplorerContents(explorer1)
 
     def test_merge(self):
-        explorer = self._explorer()
+        explorer = self._explorer("e1")
         with explorer.rebuild() as update:
             with self.db.transaction() as tr:
                 update.add_db(tr)
 
-        explorer1 = self._explorer()
+        explorer1 = self._explorer("e2")
         explorer1.set_filter({"rep_memo": "amdar"})
         with explorer1.rebuild() as update:
             update.add_explorer(explorer)
@@ -85,95 +107,71 @@ class BaseExplorerTestMixin(DballeDBMixin):
         self.assertExplorerContents(explorer1, count_unfiltered=6, count_filtered=2)
 
     def test_query_summary(self):
-        explorer = self._explorer()
-        with explorer.rebuild() as update:
-            with self.db.transaction() as tr:
-                update.add_db(tr)
-        explorer.set_filter({"rep_memo": "amdar"})
+        with self._explorer() as explorer:
+            with explorer.rebuild() as update:
+                with self.db.transaction() as tr:
+                    update.add_db(tr)
+            explorer.set_filter({"rep_memo": "amdar"})
 
-        with explorer.query_summary_all() as cur:
-            rows = list(cur)
-        self.assertEqual(len(rows), 3)
+            with explorer.query_summary_all() as cur:
+                rows = list(cur)
+            self.assertEqual(len(rows), 3)
 
-        with explorer.query_summary() as cur:
-            rows = list(cur)
-        self.assertEqual(len(rows), 1)
+            with explorer.query_summary() as cur:
+                rows = list(cur)
+            self.assertEqual(len(rows), 1)
 
-        with explorer.query_summary_all({"rep_memo": "amdar"}) as cur:
-            rows = list(cur)
-        self.assertEqual(len(rows), 1)
+            with explorer.query_summary_all({"rep_memo": "amdar"}) as cur:
+                rows = list(cur)
+            self.assertEqual(len(rows), 1)
 
     def test_create_from_msgs(self):
         importer = dballe.Importer("BUFR")
 
-        explorer = self._explorer()
-        with explorer.rebuild() as update:
-            with importer.from_file(test_pathname("bufr/gts-acars-uk1.bufr")) as imp:
-                update.add_messages(imp)
-        explorer.set_filter({"level": dballe.Level(102, 6260000)})
+        with self._explorer() as explorer:
+            with explorer.rebuild() as update:
+                with importer.from_file(test_pathname("bufr/gts-acars-uk1.bufr")) as imp:
+                    update.add_messages(imp)
+            explorer.set_filter({"level": dballe.Level(102, 6260000)})
 
-        self.assertCountEqual(explorer.all_stations, [
-            self._station("amdar", None, 48.90500, 10.63667, "EU3375"),
-        ])
-        self.assertCountEqual(explorer.stations, [
-            self._station("amdar", None, 48.90500, 10.63667, "EU3375"),
-        ])
-        self.assertEqual(explorer.all_reports, ["amdar"])
-        self.assertEqual(explorer.reports, ["amdar"])
-        self.assertEqual(explorer.all_levels, [dballe.Level(102, 6260000), None])
-        self.assertEqual(explorer.levels, [dballe.Level(102, 6260000)])
-        self.assertEqual(explorer.all_tranges, [dballe.Trange(254, 0, 0), None])
-        self.assertEqual(explorer.tranges, [dballe.Trange(254, 0, 0)])
-        self.assertCountEqual(explorer.all_varcodes, [
-            "B04001", "B04002", "B04003", "B04004", "B04005", "B01011", "B05001", "B06001",
-            "B01006", "B02061", "B02062", "B02064", "B07030", "B08004", "B11001", "B11002", "B12101", "B13002",
-        ])
-        self.assertCountEqual(explorer.varcodes, [
-            "B01006", "B02061", "B02062", "B02064", "B07030", "B08004", "B11001", "B11002", "B12101", "B13002",
-        ])
-        self.assertEqual(explorer.all_stats, dballe.ExplorerStats((
-            datetime.datetime(2009, 2, 24, 11, 31), datetime.datetime(2009, 2, 24, 11, 31), 18)))
-        self.assertEqual(explorer.stats, dballe.ExplorerStats((
-            datetime.datetime(2009, 2, 24, 11, 31), datetime.datetime(2009, 2, 24, 11, 31), 10)))
-
-    def test_persistence_json(self):
-        if os.path.exists("test-explorer.json"):
-            os.unlink("test-explorer.json")
-
-        explorer = self._explorer("test-explorer.json")
-        with explorer.rebuild() as update:
-            with self.db.transaction() as tr:
-                update.add_db(tr)
-
-        explorer = self._explorer("test-explorer.json")
-        explorer.set_filter({"rep_memo": "amdar"})
-        self.assertStrRepr(explorer)
-        self.assertExplorerContents(explorer)
+            self.assertCountEqual(explorer.all_stations, [
+                self._station("amdar", None, 48.90500, 10.63667, "EU3375"),
+            ])
+            self.assertCountEqual(explorer.stations, [
+                self._station("amdar", None, 48.90500, 10.63667, "EU3375"),
+            ])
+            self.assertEqual(explorer.all_reports, ["amdar"])
+            self.assertEqual(explorer.reports, ["amdar"])
+            self.assertEqual(explorer.all_levels, [dballe.Level(102, 6260000), None])
+            self.assertEqual(explorer.levels, [dballe.Level(102, 6260000)])
+            self.assertEqual(explorer.all_tranges, [dballe.Trange(254, 0, 0), None])
+            self.assertEqual(explorer.tranges, [dballe.Trange(254, 0, 0)])
+            self.assertCountEqual(explorer.all_varcodes, [
+                "B04001", "B04002", "B04003", "B04004", "B04005", "B01011", "B05001", "B06001",
+                "B01006", "B02061", "B02062", "B02064", "B07030", "B08004", "B11001", "B11002", "B12101", "B13002",
+            ])
+            self.assertCountEqual(explorer.varcodes, [
+                "B01006", "B02061", "B02062", "B02064", "B07030", "B08004", "B11001", "B11002", "B12101", "B13002",
+            ])
+            self.assertEqual(explorer.all_stats, dballe.ExplorerStats((
+                datetime.datetime(2009, 2, 24, 11, 31), datetime.datetime(2009, 2, 24, 11, 31), 18)))
+            self.assertEqual(explorer.stats, dballe.ExplorerStats((
+                datetime.datetime(2009, 2, 24, 11, 31), datetime.datetime(2009, 2, 24, 11, 31), 10)))
 
     def test_persistence(self):
-        if os.path.isdir("test-explorer"):
-            shutil.rmtree("test-explorer")
-        elif os.path.exists("test-explorer"):
-            os.unlink("test-explorer")
-
-        with self._explorer("test-explorer") as explorer:
+        with self._explorer() as explorer:
             with explorer.rebuild() as update:
                 with self.db.transaction() as tr:
                     update.add_db(tr)
 
-        with self._explorer("test-explorer") as explorer:
+        with self._explorer() as explorer:
             explorer.set_filter({"rep_memo": "amdar"})
             self.assertStrRepr(explorer)
             self.assertExplorerContents(explorer)
 
     def test_issue228(self):
-        if os.path.isdir("test-explorer"):
-            shutil.rmtree("test-explorer")
-        elif os.path.exists("test-explorer"):
-            os.unlink("test-explorer")
-
         # update from file
-        with self._explorer("test-explorer") as explorer:
+        with self._explorer() as explorer:
             with explorer.update() as updater:
                 importer = dballe.Importer("BUFR")
                 with importer.from_file(test_pathname("bufr/issue228.bufr")) as message:
@@ -190,10 +188,20 @@ class BaseExplorerTestMixin(DballeDBMixin):
             self.assertEqual(explorer.stats.datetime_max, None)
             self.assertEqual(explorer.stats.count, 10)
 
+    def test_issue216(self):
+        """
+        Check that one can import messages with either all data, station data
+        only, measured data only
+        """
+        if os.path.isdir("test-explorer"):
+            shutil.rmtree("test-explorer")
+        elif os.path.exists("test-explorer"):
+            os.unlink("test-explorer")
+
 
 class ExplorerTestMixin(BaseExplorerTestMixin):
-    def _explorer(self, *args, **kw):
-        return dballe.Explorer(*args, **kw)
+    def _make_explorer(self, name, *args, **kw):
+        return dballe.Explorer(name, *args, **kw)
 
     def _station(self, rep, id, lat, lon, ident):
         return dballe.Station(rep, lat, lon, ident)
@@ -204,8 +212,8 @@ class ExplorerTestMixin(BaseExplorerTestMixin):
 
 
 class DBExplorerTestMixin(BaseExplorerTestMixin):
-    def _explorer(self, *args, **kw):
-        return dballe.DBExplorer(*args, **kw)
+    def _make_explorer(self, name, *args, **kw):
+        return dballe.DBExplorer(name, *args, **kw)
 
     def _station(self, rep, id, lat, lon, ident):
         return dballe.DBStation(rep, id, lat, lon, ident)
@@ -215,11 +223,37 @@ class DBExplorerTestMixin(BaseExplorerTestMixin):
         self.assertEqual(repr(explorer), "dballe.DBExplorer object")
 
 
-class DballeV7ExplorerTest(ExplorerTestMixin, unittest.TestCase):
+class JSONExplorerTestMixin:
+    DEFAULT_EXPLORER_NAME = "test-explorer.json"
+
+    def _explorer(self, name=None, *args, **kw):
+        if name is not None and not name.endswith(".json"):
+            name += ".json"
+        return super()._explorer(name, *args, **kw)
+
+
+class XapianExplorerTestMixin:
+    DEFAULT_EXPLORER_NAME = "test-explorer"
+
+    def _explorer(self, name=None, *args, **kw):
+        if name is not None and name.endswith(".json"):
+            raise ValueError("Do not use .json extension in explorer name")
+        return super()._explorer(name, *args, **kw)
+
+
+class DballeV7ExplorerXapianTest(XapianExplorerTestMixin, unittest.TestCase):
     DB_FORMAT = "V7"
 
 
-class DballeV7DBExplorerTest(DBExplorerTestMixin, unittest.TestCase):
+class DballeV7ExplorerJSONTest(JSONExplorerTestMixin, unittest.TestCase):
+    DB_FORMAT = "V7"
+
+
+class DballeV7DBExplorerXapianTest(XapianExplorerTestMixin, DBExplorerTestMixin, unittest.TestCase):
+    DB_FORMAT = "V7"
+
+
+class DballeV7DBExplorerJSONTest(JSONExplorerTestMixin, DBExplorerTestMixin, unittest.TestCase):
     DB_FORMAT = "V7"
 
 
