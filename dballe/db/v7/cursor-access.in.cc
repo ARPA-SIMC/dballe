ff817054bfe76c494952f18eb15cf95fda7931e5
Enrico Zini <enrico@enricozini.org>
2020-08-27 15:58:45 +0200

    Use a deque for cursor results, so we deallocate during iteration. refs: #171

--- dballe/db/v7/cursor-access.in.cc
+++ dballe/db/v7/cursor-access.in.cc
@@ -24,15 +24,15 @@ void StationRows::enq(impl::Enq& enq) const
 
     switch (key) { // mklookup
         case "priority":    enq.set_int(get_priority());
-        case "rep_memo":    enq.set_string(cur->station.report);
-        case "report":      enq.set_string(cur->station.report);
-        case "ana_id":      enq.set_dballe_int(cur->station.id);
-        case "mobile":      enq.set_bool(!cur->station.ident.is_missing());
-        case "ident":       enq.set_ident(cur->station.ident);
-        case "lat":         enq.set_lat(cur->station.coords.lat);
-        case "lon":         enq.set_lon(cur->station.coords.lon);
-        case "coords":      enq.set_coords(cur->station.coords);
-        case "station":     enq.set_station(cur->station);
+        case "rep_memo":    enq.set_string(row().station.report);
+        case "report":      enq.set_string(row().station.report);
+        case "ana_id":      enq.set_dballe_int(row().station.id);
+        case "mobile":      enq.set_bool(!row().station.ident.is_missing());
+        case "ident":       enq.set_ident(row().station.ident);
+        case "lat":         enq.set_lat(row().station.coords.lat);
+        case "lon":         enq.set_lon(row().station.coords.lon);
+        case "coords":      enq.set_coords(row().station.coords);
+        case "station":     enq.set_station(row().station);
         default:            enq.search_alias_values(values());
     }
 }
@@ -44,27 +44,27 @@ void StationRows::enq(impl::Enq& enq) const
 
 void StationDataRows::enq(impl::Enq& enq) const
 {
-    if (enq.search_b_value(cur->value)) return;
+    if (enq.search_b_value(row().value)) return;
 
     const auto key = enq.key;
     const auto len = enq.len;
 
     switch (key) { // mklookup
         case "priority":    enq.set_int(get_priority());
-        case "rep_memo":    enq.set_string(cur->station.report);
-        case "report":      enq.set_string(cur->station.report);
-        case "ana_id":      enq.set_dballe_int(cur->station.id);
-        case "mobile":      enq.set_bool(!cur->station.ident.is_missing());
-        case "ident":       enq.set_ident(cur->station.ident);
-        case "lat":         enq.set_lat(cur->station.coords.lat);
-        case "lon":         enq.set_lon(cur->station.coords.lon);
-        case "coords":      enq.set_coords(cur->station.coords);
-        case "station":     enq.set_station(cur->station);
-        case "var":         enq.set_varcode(cur->value.code());
-        case "variable":    enq.set_var(cur->value.get());
-        case "attrs":       enq.set_attrs(cur->value.get());
-        case "context_id":  enq.set_dballe_int(cur->value.data_id);
-        default:            enq.search_alias_value(cur->value);
+        case "rep_memo":    enq.set_string(row().station.report);
+        case "report":      enq.set_string(row().station.report);
+        case "ana_id":      enq.set_dballe_int(row().station.id);
+        case "mobile":      enq.set_bool(!row().station.ident.is_missing());
+        case "ident":       enq.set_ident(row().station.ident);
+        case "lat":         enq.set_lat(row().station.coords.lat);
+        case "lon":         enq.set_lon(row().station.coords.lon);
+        case "coords":      enq.set_coords(row().station.coords);
+        case "station":     enq.set_station(row().station);
+        case "var":         enq.set_varcode(row().value.code());
+        case "variable":    enq.set_var(row().value.get());
+        case "attrs":       enq.set_attrs(row().value.get());
+        case "context_id":  enq.set_dballe_int(row().value.data_id);
+        default:            enq.search_alias_value(row().value);
     }
 }
 
@@ -74,29 +74,29 @@ void StationDataRows::enq(impl::Enq& enq) const
 
 void BaseDataRows::enq(impl::Enq& enq) const
 {
-    if (enq.search_b_value(cur->value)) return;
+    if (enq.search_b_value(row().value)) return;
 
     const auto key = enq.key;
     const auto len = enq.len;
 
     switch (key) { // mklookup
         case "priority":    enq.set_int(get_priority());
-        case "rep_memo":    enq.set_string(cur->station.report);
-        case "report":      enq.set_string(cur->station.report);
-        case "ana_id":      enq.set_dballe_int(cur->station.id);
-        case "mobile":      enq.set_bool(!cur->station.ident.is_missing());
-        case "ident":       enq.set_ident(cur->station.ident);
-        case "lat":         enq.set_lat(cur->station.coords.lat);
-        case "lon":         enq.set_lon(cur->station.coords.lon);
-        case "coords":      enq.set_coords(cur->station.coords);
-        case "station":     enq.set_station(cur->station);
-        case "datetime":    enq.set_datetime(cur->datetime);
-        case "year":        enq.set_int(cur->datetime.year);
-        case "month":       enq.set_int(cur->datetime.month);
-        case "day":         enq.set_int(cur->datetime.day);
-        case "hour":        enq.set_int(cur->datetime.hour);
-        case "min":         enq.set_int(cur->datetime.minute);
-        case "sec":         enq.set_int(cur->datetime.second);
+        case "rep_memo":    enq.set_string(row().station.report);
+        case "report":      enq.set_string(row().station.report);
+        case "ana_id":      enq.set_dballe_int(row().station.id);
+        case "mobile":      enq.set_bool(!row().station.ident.is_missing());
+        case "ident":       enq.set_ident(row().station.ident);
+        case "lat":         enq.set_lat(row().station.coords.lat);
+        case "lon":         enq.set_lon(row().station.coords.lon);
+        case "coords":      enq.set_coords(row().station.coords);
+        case "station":     enq.set_station(row().station);
+        case "datetime":    enq.set_datetime(row().datetime);
+        case "year":        enq.set_int(row().datetime.year);
+        case "month":       enq.set_int(row().datetime.month);
+        case "day":         enq.set_int(row().datetime.day);
+        case "hour":        enq.set_int(row().datetime.hour);
+        case "min":         enq.set_int(row().datetime.minute);
+        case "sec":         enq.set_int(row().datetime.second);
         case "level":       enq.set_level(get_levtr().level);
         case "leveltype1":  enq.set_dballe_int(get_levtr().level.ltype1);
         case "l1":          enq.set_dballe_int(get_levtr().level.l1);
@@ -106,11 +106,11 @@ void BaseDataRows::enq(impl::Enq& enq) const
         case "pindicator":  enq.set_dballe_int(get_levtr().trange.pind);
         case "p1":          enq.set_dballe_int(get_levtr().trange.p1);
         case "p2":          enq.set_dballe_int(get_levtr().trange.p2);
-        case "var":         enq.set_varcode(cur->value.code());
-        case "variable":    enq.set_var(cur->value.get());
-        case "attrs":       enq.set_attrs(cur->value.get());
-        case "context_id":  enq.set_dballe_int(cur->value.data_id);
-        default:            enq.search_alias_value(cur->value);
+        case "var":         enq.set_varcode(row().value.code());
+        case "variable":    enq.set_var(row().value.get());
+        case "attrs":       enq.set_attrs(row().value.get());
+        case "context_id":  enq.set_dballe_int(row().value.data_id);
+        default:            enq.search_alias_value(row().value);
     }
 }
 
@@ -125,29 +125,29 @@ void SummaryRows::enq(impl::Enq& enq) const
 
     switch (key) { // mklookup
         case "priority":    enq.set_int(get_priority());
-        case "rep_memo":    enq.set_string(cur->station.report);
-        case "report":      enq.set_string(cur->station.report);
-        case "ana_id":      enq.set_dballe_int(cur->station.id);
-        case "mobile":      enq.set_bool(!cur->station.ident.is_missing());
-        case "ident":       enq.set_ident(cur->station.ident);
-        case "lat":         enq.set_lat(cur->station.coords.lat);
-        case "lon":         enq.set_lon(cur->station.coords.lon);
-        case "coords":      enq.set_coords(cur->station.coords);
-        case "station":     enq.set_station(cur->station);
-        case "datetimemax": if (cur->dtrange.is_missing()) return; else enq.set_datetime(cur->dtrange.max);
-        case "datetimemin": if (cur->dtrange.is_missing()) return; else enq.set_datetime(cur->dtrange.min);
-        case "yearmax":     if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.max.year);
-        case "yearmin":     if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.min.year);
-        case "monthmax":    if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.max.month);
-        case "monthmin":    if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.min.month);
-        case "daymax":      if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.max.day);
-        case "daymin":      if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.min.day);
-        case "hourmax":     if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.max.hour);
-        case "hourmin":     if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.min.hour);
-        case "minumax":     if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.max.minute);
-        case "minumin":     if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.min.minute);
-        case "secmax":      if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.max.second);
-        case "secmin":      if (cur->dtrange.is_missing()) return; else enq.set_int(cur->dtrange.min.second);
+        case "rep_memo":    enq.set_string(row().station.report);
+        case "report":      enq.set_string(row().station.report);
+        case "ana_id":      enq.set_dballe_int(row().station.id);
+        case "mobile":      enq.set_bool(!row().station.ident.is_missing());
+        case "ident":       enq.set_ident(row().station.ident);
+        case "lat":         enq.set_lat(row().station.coords.lat);
+        case "lon":         enq.set_lon(row().station.coords.lon);
+        case "coords":      enq.set_coords(row().station.coords);
+        case "station":     enq.set_station(row().station);
+        case "datetimemax": if (row().dtrange.is_missing()) return; else enq.set_datetime(row().dtrange.max);
+        case "datetimemin": if (row().dtrange.is_missing()) return; else enq.set_datetime(row().dtrange.min);
+        case "yearmax":     if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.max.year);
+        case "yearmin":     if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.min.year);
+        case "monthmax":    if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.max.month);
+        case "monthmin":    if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.min.month);
+        case "daymax":      if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.max.day);
+        case "daymin":      if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.min.day);
+        case "hourmax":     if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.max.hour);
+        case "hourmin":     if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.min.hour);
+        case "minumax":     if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.max.minute);
+        case "minumin":     if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.min.minute);
+        case "secmax":      if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.max.second);
+        case "secmin":      if (row().dtrange.is_missing()) return; else enq.set_int(row().dtrange.min.second);
         case "level":       enq.set_level(get_levtr().level);
         case "leveltype1":  enq.set_dballe_int(get_levtr().level.ltype1);
         case "l1":          enq.set_dballe_int(get_levtr().level.l1);
@@ -157,9 +157,9 @@ void SummaryRows::enq(impl::Enq& enq) const
         case "pindicator":  enq.set_dballe_int(get_levtr().trange.pind);
         case "p1":          enq.set_dballe_int(get_levtr().trange.p1);
         case "p2":          enq.set_dballe_int(get_levtr().trange.p2);
-        case "var":         enq.set_varcode(cur->code);
-        case "context_id":  enq.set_int(cur->count);
-        case "count":       enq.set_int(cur->count);
+        case "var":         enq.set_varcode(row().code);
+        case "context_id":  enq.set_int(row().count);
+        case "count":       enq.set_int(row().count);
         default:            wreport::error_notfound::throwf("key %s not found on this query result", key);
     }
 }
