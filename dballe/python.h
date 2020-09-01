82e074df2b76f880da40190640396030a2b38c27
Enrico Zini <enrico@enricozini.org>
2020-09-01 13:20:01 +0200

    Removed leftover comments from wreport code used as boilerplate. refs: #171

--- dballe/python.h
+++ dballe/python.h
@@ -23,7 +23,7 @@ extern "C" {
  *
  * To use them, retrieve a pointer to the struct via the Capsule system:
  * \code
- * dbapy_c_api* wrpy = (dbapy_c_api*)PyCapsule_Import("_dballe._C_API", 0);
+ * dbapy_c_api* dbapy = (dbapy_c_api*)PyCapsule_Import("_dballe._C_API", 0);
  * \endcode
  * 
  */
@@ -45,55 +45,6 @@ struct dbapy_c_api {
 
     /// Create a dballe.Message referencing the given message
     PyObject* (*message_create)(std::shared_ptr<dballe::Message>);
-
-#if 0
-    /// Create a new unset wreport.Var object
-    PyObject* (*var_create)(const wreport::Varinfo&);
-
-    /// Create a new wreport.Var object with an integer value
-    PyObject* (*var_create_i)(const wreport::Varinfo&, int);
-
-    /// Create a new wreport.Var object with a double value
-    PyObject* (*var_create_d)(const wreport::Varinfo&, double);
-
-    /// Create a new wreport.Var object with a C string value
-    PyObject* (*var_create_c)(const wreport::Varinfo&, const char*);
-
-    /// Create a new wreport.Var object with a std::string value
-    PyObject* (*var_create_s)(const wreport::Varinfo&, const std::string&);
-
-    /// Create a new wreport.Var object as a copy of an existing var
-    PyObject* (*var_create_copy)(const wreport::Var&);
-
-    /// Read the value of a variable as a new Python object
-    PyObject* (*var_value_to_python)(const wreport::Var&);
-
-    /// Set the value of a variable from a Python object (borrowed reference)
-    int (*var_value_from_python)(PyObject* o, wreport::Var&);
-
-    /// Create a wreport.Varinfo object from a C++ Varinfo
-    PyObject* (*varinfo_create)(wreport::Varinfo);
-
-    /// Create a wreport:Vartable object from a C++ Vartable
-    PyObject* (*vartable_create)(const wreport::Vartable*);
-
-    /// Vartable type
-    PyTypeObject* vartable_type;
-
-    /// Var type
-    PyTypeObject* var_type;
-
-// API version 1.1
-
-    /// Create a new wreport.Var object, moving an existing var
-    PyObject* (*var_create_move)(wreport::Var&&);
-
-    /// Return the variable for a wreport.Var object
-    wreport:: Var* (*var)(PyObject* o);
-
-    /// Create a new wreport.Var object with the value from another variable
-    PyObject* (*var_create_v)(const wreport::Varinfo&, const wreport::Var&);
-#endif
 };
 
 }
